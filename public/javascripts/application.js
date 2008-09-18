d2772d33662599f6f05e1cba8ca2d437256c1b9d
Carmelyne Thompson <carmelyne@hashrocket.com>
2008-09-18 13:58:23 -0400

    desi and carm's commit for ui elements and images

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// Flash dismiss
+$('.flash .dismiss a').livequery('click', function(event) {
+  $(this).parents('.flash').slideUp();
+  return false;
+});
+
+// IE PNG fix
+$(document).ready(function(){ 
+  $(document).pngFix(); 
+});
