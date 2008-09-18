e823517041b663107f8baa64ad333b52f3fed238
erik <desi@hashrocket.com>
2008-09-18 17:52:14 -0400

    Desi - removing footnotes for real and fixing the application.js loading issue (thanks for help durran)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-
-// Flash dismiss
-$('.flash .dismiss a').livequery('click', function(event) {
+$(document).ready(function(){ 
+	$('#equalize').equalHeights();
+	$(document).pngFix();
+	$("#content_tabbed > ul").tabs();
+});
+// Flash dismiss - CARM TALK TO DESI WE ARE MISSING jslivequery.js for this
+/*$('.flash .dismiss a').livequery('click', function(event) {
   $(this).parents('.flash').slideUp();
   return false;
-});
+});*/
 
-// IE PNG fix
-$(document).ready(function(){ 
-  $(document).pngFix(); 
-});
+
