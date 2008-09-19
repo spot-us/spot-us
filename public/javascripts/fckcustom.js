7745554bfe42e36bd237171446ab8be61f7f86d2
Carmelyne Thompson <carmelyne@hashrocket.com>
2008-09-19 16:14:51 -0400

    add .DS_store to ignore, config settings for FCKEditor config file

--- public/javascripts/fckcustom.js
+++ public/javascripts/fckcustom.js
@@ -1,24 +1,38 @@
+// SPOTUS setting for FCKEditor
+
 // CHANGE FOR APPS HOSTED IN SUBDIRECTORY
 FCKRelativePath = '';
-//
-// FCKConfig.LinkBrowser = false;
-FCKConfig.ImageBrowser = false;
-FCKConfig.FlashBrowser = false;
+
+// section modified image dialog
+
+FCKConfig.LinkDlgHideTarget     = true ;
+FCKConfig.LinkDlgHideAdvanced   = true ;
+FCKConfig.ImageDlgHideLink      = true ;
+FCKConfig.ImageDlgHideAdvanced  = true ;
+FCKConfig.FlashDlgHideAdvanced  = true ;
+
+FCKConfig.LinkBrowser   = false ;
+FCKConfig.ImageBrowser  = false ;
+FCKConfig.FlashBrowser  = false ;
+FCKConfig.LinkUpload    = false ;
+FCKConfig.ImageUpload   = false ;
+FCKConfig.FlashUpload   = false ;
 
 // DON'T CHANGE THESE
-FCKConfig.LinkBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Connector='+FCKRelativePath+'/fckeditor/command';
-FCKConfig.ImageBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Type=Image&Connector='+FCKRelativePath+'/fckeditor/command';
-FCKConfig.FlashBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Type=Flash&Connector='+FCKRelativePath+'/fckeditor/command';
-
-FCKConfig.LinkUploadURL = FCKRelativePath+'/fckeditor/upload';
-FCKConfig.ImageUploadURL = FCKRelativePath+'/fckeditor/upload?Type=Image';
-FCKConfig.FlashUploadURL = FCKRelativePath+'/fckeditor/upload?Type=Flash';
-FCKConfig.SpellerPagesServerScript = FCKRelativePath+'/fckeditor/check_spelling';
-FCKConfig.AllowQueryStringDebug = false;
-FCKConfig.SpellChecker = 'SpellerPages';
+// FCKConfig.LinkBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Connector='+FCKRelativePath+'/fckeditor/command';
+// FCKConfig.ImageBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Type=Image&Connector='+FCKRelativePath+'/fckeditor/command';
+// FCKConfig.FlashBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Type=Flash&Connector='+FCKRelativePath+'/fckeditor/command';
+// 
+// FCKConfig.LinkUploadURL = FCKRelativePath+'/fckeditor/upload';
+// FCKConfig.ImageUploadURL = FCKRelativePath+'/fckeditor/upload?Type=Image';
+// FCKConfig.FlashUploadURL = FCKRelativePath+'/fckeditor/upload?Type=Flash';
+// FCKConfig.SpellerPagesServerScript = FCKRelativePath+'/fckeditor/check_spelling';
+// FCKConfig.AllowQueryStringDebug = false;
+// FCKConfig.SpellChecker = 'SpellerPages';
 
 // ONLY CHANGE BELOW HERE
 FCKConfig.SkinPath = FCKConfig.BasePath + 'skins/silver/';
+FCKConfig.RemoveFormatTags = 'b,big,code,del,dfn,em,font,i,ins,kbd,q,samp,small,span,strike,strong,sub,sup,tt,u,var';
 
 FCKConfig.ToolbarSets["Simple"] = [
 	['Source','-','-','Templates'],
