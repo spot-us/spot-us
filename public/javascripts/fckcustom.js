1052384f029328cf85a38f90a0e898f8824c9fda
GIT_AUTHOR_NAME <carmelyne@hashrocket.com>
2008-09-20 19:10:55 -0400

    ui elements for 3-2-1 sprint

--- public/javascripts/fckcustom.js
+++ public/javascripts/fckcustom.js
@@ -3,7 +3,7 @@
 // CHANGE FOR APPS HOSTED IN SUBDIRECTORY
 FCKRelativePath = '';
 
-// section modified image dialog
+// section modified image dialog without the upload and advanced tabs
 
 FCKConfig.LinkDlgHideTarget     = true ;
 FCKConfig.LinkDlgHideAdvanced   = true ;
@@ -18,37 +18,16 @@ FCKConfig.LinkUpload    = false ;
 FCKConfig.ImageUpload   = false ;
 FCKConfig.FlashUpload   = false ;
 
-// DON'T CHANGE THESE
-// FCKConfig.LinkBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Connector='+FCKRelativePath+'/fckeditor/command';
-// FCKConfig.ImageBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Type=Image&Connector='+FCKRelativePath+'/fckeditor/command';
-// FCKConfig.FlashBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Type=Flash&Connector='+FCKRelativePath+'/fckeditor/command';
-// 
-// FCKConfig.LinkUploadURL = FCKRelativePath+'/fckeditor/upload';
-// FCKConfig.ImageUploadURL = FCKRelativePath+'/fckeditor/upload?Type=Image';
-// FCKConfig.FlashUploadURL = FCKRelativePath+'/fckeditor/upload?Type=Flash';
-// FCKConfig.SpellerPagesServerScript = FCKRelativePath+'/fckeditor/check_spelling';
-// FCKConfig.AllowQueryStringDebug = false;
-// FCKConfig.SpellChecker = 'SpellerPages';
+// disable toolbar collapse
+FCKConfig.ToolbarCanCollapse = false;
 
-// ONLY CHANGE BELOW HERE
+// silver theme was modified to fit spotus.us theme
 FCKConfig.SkinPath = FCKConfig.BasePath + 'skins/silver/';
+
+// removes formatted tags copied into the textarea
 FCKConfig.RemoveFormatTags = 'b,big,code,del,dfn,em,font,i,ins,kbd,q,samp,small,span,strike,strong,sub,sup,tt,u,var';
 
-FCKConfig.ToolbarSets["Simple"] = [
-	['Source','-','-','Templates'],
-	['Cut','Copy','Paste','PasteWord','-','Print','SpellCheck'],
-	['Undo','Redo','-','Find','Replace','-','SelectAll'],
-	'/',
-	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
-	['OrderedList','UnorderedList','-','Outdent','Indent'],
-	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
-	['Link','Unlink'],
-	'/',
-	['Image','Table','Rule','Smiley'],
-	['FontName','FontSize'],
-	['TextColor','BGColor'],
-	['-','About']
-] ;
+// toolbar specific to spot.us
 FCKConfig.ToolbarSets["Spotus"] = [
 	['Bold','Italic','OrderedList','UnorderedList','-','Outdent','Indent','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull','-','Link','Rule','Source'],
 ] ;
