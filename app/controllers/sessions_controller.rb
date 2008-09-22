4a6b19a10a841e79af4238588d52b0fdce7d89df
GIT_COMMITTER_NAME <desi@hashrocket.com>
2008-09-22 16:10:57 -0400

    Joe + Desi: refactored error handling for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,9 +9,7 @@ class SessionsController < ApplicationController
         cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
       end
       redirect_back_or_default('/')
-      flash[:notice] = "Logged in successfully"
     else
-      flash[:error] = render_to_string(:partial => 'sessions/error')
       render :action => 'new'
     end
   end
