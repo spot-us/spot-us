6a2ad8f1bc0eb1e1c94ed440f91710436225484f
GIT_COMMITTER_NAME <jonlarkowski@gmail.com>
2008-09-22 17:06:33 -0400

    added factory_girl (+tsaleh)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'spec'
 require 'spec/rails'
+require 'factory_girl'
 
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
