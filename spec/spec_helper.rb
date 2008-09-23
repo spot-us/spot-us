456a442723c89e06438bcd4eba8b39fd80211434
Jon Larkowski <jonlarkowski@gmail.com>
2008-09-23 10:53:16 -0400

    Added routing spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,3 +34,13 @@ def table_has_columns(clazz, type, *column_names)
   end
 end
 
+def route_matches(path, method, params)
+  it "maps #{params.inspect} to #{path.inspect}" do
+    route_for(params).should == path
+  end
+
+  it "generates params #{params.inspect} from #{method.to_s.upcase} to #{path.inspect}" do
+    params_from(method.to_sym, path).should == params
+  end
+end
+
