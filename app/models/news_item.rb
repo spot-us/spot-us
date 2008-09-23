c1f8afc5c0654b7be75865527d15bdafd5355362
erik <jonlarkowski@gmail.com>
2008-09-23 16:53:15 -0400

    Added validations to NewsItem and Pitch (+tsaleh)

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,4 +1,6 @@
 class NewsItem < ActiveRecord::Base
+  validates_presence_of :location, :headline
+
   has_attached_file :featured_image, 
                     :styles => { :thumb => '50x50#', :medium => "250x150#" }, 
                     :path => ":rails_root/public/system/news_items/:attachment/:id_partition/:basename_:style.:extension",
