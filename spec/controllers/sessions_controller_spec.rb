10c810414768b13602adf542638b55b809ddd9d7
GIT_AUTHOR_NAME <desi@hashrocket.com>
2008-09-23 10:23:06 -0400

    Joe + Desi: replaced fixtures with factories

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,7 +5,6 @@ require File.dirname(__FILE__) + '/../spec_helper'
 include AuthenticatedTestHelper
 
 describe SessionsController do
-  fixtures :users
 
   describe "on POST to create with a bad login" do
     before do
@@ -28,57 +27,64 @@ describe SessionsController do
   end
 
   it 'logins and redirects' do
-    post :create, :email => 'quentin@example.com', :password => 'test'
+    Factory(:user, :email => 'user@example.com', :password => 'test')
+    post :create, :email => 'user@example.com', :password => 'test'
     session[:user_id].should_not be_nil
     response.should be_redirect
   end
   
   it 'fails login and does not redirect' do
-    post :create, :email => 'quentin@example.com', :password => 'bad password'
+    Factory(:user, :email => 'user@example.com', :password => 'test')
+    post :create, :email => 'user@example.com', :password => 'bad password'
     session[:user_id].should be_nil
     response.should be_success
   end
 
   it 'logs out' do
-    login_as :quentin
+    login_as Factory(:user)
     get :destroy
     session[:user_id].should be_nil
     response.should be_redirect
   end
 
   it 'remembers me' do
-    post :create, :email => 'quentin@example.com', :password => 'test', :remember_me => "1"
+    Factory(:user, :email => 'user@example.com', :password => 'test')
+    post :create, :email => 'user@example.com', :password => 'test', :remember_me => "1"
     response.cookies["auth_token"].should_not be_nil
   end
   
   it 'does not remember me' do
-    post :create, :login => 'quentin', :password => 'test', :remember_me => "0"
+    Factory(:user, :email => 'user@example.com', :password => 'test')
+    post :create, :email => 'user@example.com', :password => 'test', :remember_me => "0"
     response.cookies["auth_token"].should be_nil
   end
 
   it 'deletes token on logout' do
-    login_as :quentin
+    login_as Factory(:user)
     get :destroy
     response.cookies["auth_token"].should == []
   end
 
   it 'logs in with cookie' do
-    users(:quentin).remember_me
-    request.cookies["auth_token"] = cookie_for(:quentin)
+    user = Factory(:user)
+    user.remember_me
+    request.cookies["auth_token"] = cookie_for(user)
     get :new
     controller.send(:logged_in?).should be_true
   end
   
   it 'fails expired cookie login' do
-    users(:quentin).remember_me
-    users(:quentin).update_attribute :remember_token_expires_at, 5.minutes.ago
-    request.cookies["auth_token"] = cookie_for(:quentin)
+    user = Factory(:user)
+    user.remember_me
+    user.update_attribute :remember_token_expires_at, 5.minutes.ago
+    request.cookies["auth_token"] = cookie_for(user)
     get :new
     controller.send(:logged_in?).should_not be_true
   end
   
   it 'fails cookie login' do
-    users(:quentin).remember_me
+    user = Factory(:user)
+    user.remember_me
     request.cookies["auth_token"] = auth_token('invalid_auth_token')
     get :new
     controller.send(:logged_in?).should_not be_true
@@ -89,6 +95,6 @@ describe SessionsController do
   end
     
   def cookie_for(user)
-    auth_token users(user).remember_token
+    auth_token user.remember_token
   end
 end
