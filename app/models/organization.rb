80b754ca0a5edbda18cbee1037ec33a36286dbb9
GIT_COMMITTER_NAME <jonlarkowski@gmail.com>
2008-09-23 13:18:56 -0400

    rake annotate_models

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                        :integer(4)      not null, primary key
+#  email                     :string(255)     
+#  crypted_password          :string(40)      
+#  salt                      :string(40)      
+#  created_at                :datetime        
+#  updated_at                :datetime        
+#  remember_token            :string(255)     
+#  remember_token_expires_at :datetime        
+#  first_name                :string(255)     
+#  last_name                 :string(255)     
+#  type                      :string(255)     
+#
+
 class Organization < User
 end
