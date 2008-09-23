062be2dd4010533d3bdd0ec8deff97425d87fa9d
GIT_COMMITTER_NAME <jonlarkowski@gmail.com>
2008-09-23 10:29:15 -0400

    tsaleh added a 'new' 'line' :)

--- config/environment.rb
+++ config/environment.rb
@@ -25,3 +25,4 @@ Rails::Initializer.run do |config|
     :secret      => '5364db1b81e0c120704c572a45f1c6929d10c54ea5b4069680f396e898d8af69920ad7ec1524c5a8f32340624c61f4232204d299b484108bbcdefed1a26f7e7a'
   }
 end
+
