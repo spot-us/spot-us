2dd359c3549ff42284e21c1ecbc06c5f888fe4dc
GIT_COMMITTER_NAME <desi@hashrocket.com>
2008-09-23 16:42:41 -0400

    Joe + Desi: added the controller path as a CSS class to body, and styled myspot navigation tabs based on the current controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def body_class
+    controller.controller_path.underscore.gsub('/', '_')
+  end
+
 end
