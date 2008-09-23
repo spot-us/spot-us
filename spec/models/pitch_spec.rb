088d63f529774f954e98d1bdb910dce5f3478aac
Jon Larkowski <jonlarkowski@gmail.com>
2008-09-23 14:59:55 -0400

    Converted Pitch to STI against NewsItem (+tsaleh)

--- spec/models/pitch_spec.rb
+++ spec/models/pitch_spec.rb
@@ -1,20 +1,22 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Pitch do
-  table_has_columns(Pitch, :string, *%w(headline location requested_amount))
-  table_has_columns(Pitch, :text, *%w(short_description delivery_description extended_description skills keywords))
-  table_has_columns(Pitch, :boolean, *%w(deliver_text deliver_audio deliver_video deliver_photo contract_agreement))
-  table_has_columns(Pitch, :datetime, *%w(expiration_date))
+  table_has_columns(Pitch, :string,   "requested_amount")
+  table_has_columns(Pitch, :text,     "short_description")
+  table_has_columns(Pitch, :text,     "delivery_description")
+  table_has_columns(Pitch, :text,     "extended_description")
+  table_has_columns(Pitch, :text,     "skills")
+  table_has_columns(Pitch, :boolean,  "deliver_text")
+  table_has_columns(Pitch, :boolean,  "deliver_audio")
+  table_has_columns(Pitch, :boolean,  "deliver_video")
+  table_has_columns(Pitch, :boolean,  "deliver_photo")
+  table_has_columns(Pitch, :boolean,  "contract_agreement")
+  table_has_columns(Pitch, :datetime, "expiration_date")
 
-  describe "to support paperclip" do
-    it "has a paperclip attachment for featured_image" do
-      Factory(:pitch).featured_image.should be_instance_of(Paperclip::Attachment)
+  describe "to support STI" do
+    it "descends from NewItem" do
+      Pitch.ancestors.include?(NewsItem)
     end
-
-    table_has_columns(Pitch, :string, :featured_image_file_name)
-    table_has_columns(Pitch, :string, :featured_image_content_type)
-    table_has_columns(Pitch, :integer, :featured_image_file_size)
-    table_has_columns(Pitch, :datetime, :featured_image_updated_at)
   end
 end
 
