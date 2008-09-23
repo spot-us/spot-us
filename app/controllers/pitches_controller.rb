b64bbd43501bfb0be8f794708ab7a4346c027046
erik <jonlarkowski@gmail.com>
2008-09-23 19:31:48 -0400

    Added donations and pitch edit secrity (+tsaleh)

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -1,8 +1,12 @@
 class PitchesController < ApplicationController
-  # before_filter :block_if_donated_to, :only => :edit
+  before_filter :block_if_donated_to, :only => :edit
   resources_controller_for :pitch
 
-  # def block_if_donated_to
-  #   access_denied if find_resource(params[:id]).donated_to?
-  # end
+  def block_if_donated_to
+    pitch = find_resource(params[:id])
+    if pitch.donated_to?
+      access_denied(:flash => "You cannot edit a pitch that has donations.", 
+                    :redirect => pitch_url(pitch)) 
+    end
+  end
 end
