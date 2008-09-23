1da3878bdabc99e2fd77bb5a6bda313199afff20
GIT_COMMITTER_NAME <jonlarkowski@gmail.com>
2008-09-23 18:46:08 -0400

    Added pitches/edit

--- spec/views/pitches/edit.html.haml_spec.rb
+++ spec/views/pitches/edit.html.haml_spec.rb
@@ -4,15 +4,18 @@ describe "/pitches/edit.html.haml" do
   include PitchesHelper
   
   before do
-    @pitch = mock_model(Pitch)
+    @pitch = Factory(:pitch)
     assigns[:pitch] = @pitch
   end
 
   it "should render edit form" do
     render "/pitches/edit.html.haml"
-    
-    response.should have_tag("form[action=#{pitch_path(@pitch)}][method=post]") do
-    end
+    response.should have_tag("form[action=#{pitch_path(@pitch)}][method=post]") 
+  end
+
+  it "should render _form partial" do
+    template.expect_render(:partial => "form")
+    render "/pitches/edit.html.haml"
   end
 end
 
