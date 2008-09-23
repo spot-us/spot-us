a62e9d87eba7936c4db80b11ff65cf29033c8e64
GIT_AUTHOR_NAME <jonlarkowski@gmail.com>
2008-09-23 12:40:39 -0400

    First pass at pitches/new (+tsaleh)

--- spec/models/pitch_spec.rb
+++ spec/models/pitch_spec.rb
@@ -2,11 +2,8 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Pitch do
   table_has_columns(Pitch, :string, *%w(headline location requested_amount))
-
-  %w(short_description delivery_description extened_desciption skills keywords)
-
-  %w(text_delivery audio_delivery video_delivery photo_delivery contract_agreement)
-
-  %w(expiration_date)
+  table_has_columns(Pitch, :text, *%w(short_description delivery_description extended_description skills keywords))
+  table_has_columns(Pitch, :boolean, *%w(deliver_text deliver_audio deliver_video deliver_photo contract_agreement))
+  table_has_columns(Pitch, :datetime, *%w(expiration_date))
 end
 
--- spec/views/pitches/new.html.haml_spec.rb
+++ spec/views/pitches/new.html.haml_spec.rb
@@ -4,7 +4,7 @@ describe "/pitches/new.html.haml" do
   include PitchesHelper
   
   before(:each) do
-    @pitch = mock_model(Pitch)
+    @pitch = stub_model(Pitch)
     @pitch.stub!(:new_record?).and_return(true)
     assigns[:pitch] = @pitch
   end
@@ -13,9 +13,46 @@ describe "/pitches/new.html.haml" do
     render "/pitches/new.html.haml"
     
     response.should have_tag("form[action=?][method=post]", pitches_path) do
-
+      with_tag "input[type=submit]"
     end
   end
+
+  %w(requested_amount).each do |field|
+    it "renders #{field} as text" do
+      render "/pitches/new.html.haml"
+      response.should have_tag("form") do
+        with_tag "input[type=text][name=?]", "pitch[#{field}]"
+      end
+    end
+  end
+
+  %w(location).each do |field|
+    it "renders #{field} as select" do
+      render "/pitches/new.html.haml"
+      response.should have_tag("form") do
+        with_tag "select[name=?]", "pitch[#{field}]"
+      end
+    end
+  end
+
+  %w(headline short_description delivery_description extended_description skills keywords).each do |field|
+    it "renders #{field} as textarea" do
+      render "/pitches/new.html.haml"
+      response.should have_tag("form") do
+        with_tag "textarea[name=?]", "pitch[#{field}]"
+      end
+    end
+  end
+
+  %w(deliver_text deliver_audio deliver_video deliver_photo contract_agreement).each do |field|
+    it "renders #{field} as checkbox" do
+      render "/pitches/new.html.haml"
+      response.should have_tag("form") do
+        with_tag "input[type=checkbox][name=?]", "pitch[#{field}]"
+      end
+    end
+  end
+
 end
 
 
