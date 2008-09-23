491b23ac7761a9ca01c5ea89dd43914ae3e31663
GIT_AUTHOR_NAME <jonlarkowski@gmail.com>
2008-09-23 18:46:08 -0400

    Added pitches/edit

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -1,3 +1,8 @@
 class PitchesController < ApplicationController
+  # before_filter :block_if_donated_to, :only => :edit
   resources_controller_for :pitch
+
+  # def block_if_donated_to
+  #   access_denied if find_resource(params[:id]).donated_to?
+  # end
 end
