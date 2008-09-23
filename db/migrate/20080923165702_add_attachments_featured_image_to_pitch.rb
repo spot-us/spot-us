39171557dd10e2953b82b31748dafaf60cbb6716
Jon Larkowski <jonlarkowski@gmail.com>
2008-09-23 13:11:47 -0400

    added paperclip field for featured image (+tsaleh)

---
+++ db/migrate/20080923165702_add_attachments_featured_image_to_pitch.rb
@@ -0,0 +1,15 @@
+class AddAttachmentsFeaturedImageToPitch < ActiveRecord::Migration
+  def self.up
+    add_column :pitches, :featured_image_file_name, :string
+    add_column :pitches, :featured_image_content_type, :string
+    add_column :pitches, :featured_image_file_size, :integer
+    add_column :pitches, :featured_image_updated_at, :datetime
+  end
+
+  def self.down
+    remove_column :pitches, :featured_image_file_name
+    remove_column :pitches, :featured_image_content_type
+    remove_column :pitches, :featured_image_file_size
+    remove_column :pitches, :featured_image_updated_at
+  end
+end
