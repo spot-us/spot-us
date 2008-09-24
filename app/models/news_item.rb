d3f07903d2a9ec68e64c8761d60b30373582af86
GIT_AUTHOR_NAME <desi@hashrocket.com>
2008-09-24 13:04:41 -0400

    Desi and Tammer: tips/show and user association moved to news item

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,4 +1,6 @@
 class NewsItem < ActiveRecord::Base
+  belongs_to :user
+  
   has_attached_file :featured_image, 
                     :styles => { :thumb => '50x50#', :medium => "250x150#" }, 
                     :path => ":rails_root/public/system/news_items/:attachment/:id_partition/:basename_:style.:extension",
@@ -10,4 +12,8 @@ class NewsItem < ActiveRecord::Base
     :content_type => ['image/jpeg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png', 'image/jpg'], 
     :message => "Oops! Make sure you are uploading an image file." 
   validates_attachment_size :featured_image, :in => 1..5.megabytes
+
+  def editable_by?(user)
+    self.user == user
+  end
 end
