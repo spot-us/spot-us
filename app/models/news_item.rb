f77981ec44a269addab643c45339cea82ef05675
GIT_AUTHOR_NAME <desi@hashrocket.com>
2008-09-24 17:42:50 -0400

    Desi and Tammer: fixing a couple of things with the tips views

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -2,7 +2,7 @@ class NewsItem < ActiveRecord::Base
   belongs_to :user
   
   has_attached_file :featured_image, 
-                    :styles => { :thumb => '50x50#', :medium => "250x150#" }, 
+                    :styles => { :thumb => '50x50#', :medium => "250x250#" }, 
                     :path => ":rails_root/public/system/news_items/:attachment/:id_partition/:basename_:style.:extension",
                     :url =>                    "/system/news_items/:attachment/:id_partition/:basename_:style.:extension"
   
