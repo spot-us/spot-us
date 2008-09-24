752161a06550d293cfe6db2f8f24f5203ca5d40d
erik <jonlarkowski@gmail.com>
2008-09-24 14:19:39 -0400

    Added the purchase model (handles receipts, donation statuses, credit card transactions) (+jferris)

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,6 +1,7 @@
 class Donation < ActiveRecord::Base
   belongs_to :user
   belongs_to :pitch
+  belongs_to :purchase
   validates_presence_of :pitch_id
   validates_presence_of :user_id
   validates_presence_of :amount_in_cents
