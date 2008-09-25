a0860124f00ec0cdc67b2045ea913e13994afb04
Jon Larkowski <jonlarkowski@gmail.com>
2008-09-24 20:08:11 -0400

    Updated model annotations (+jferris)

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: donations
+#
+#  id              :integer(4)      not null, primary key
+#  user_id         :integer(4)      
+#  pitch_id        :integer(4)      
+#  created_at      :datetime        
+#  updated_at      :datetime        
+#  amount_in_cents :integer(4)      
+#  paid            :boolean(1)      not null
+#  purchase_id     :integer(4)      
+#
+
 class Donation < ActiveRecord::Base
   belongs_to :user
   belongs_to :pitch
--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: pledges
+#
+#  id              :integer(4)      not null, primary key
+#  user_id         :integer(4)      
+#  tip_id          :integer(4)      
+#  amount_in_cents :integer(4)      
+#  created_at      :datetime        
+#  updated_at      :datetime        
+#
+
 class Pledge < ActiveRecord::Base
   belongs_to :user
   belongs_to :tip
