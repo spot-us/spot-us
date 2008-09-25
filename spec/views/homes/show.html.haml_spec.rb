ed7e6cbe27dbdfdd504ce626e37a89ace928c01c
Jon Larkowski <jonlarkowski@gmail.com>
2008-09-25 19:44:39 -0400

    Added a check to ensure that the featured pitch isn't accessed when nil (+jferris)

--- spec/views/homes/show.html.haml_spec.rb
+++ spec/views/homes/show.html.haml_spec.rb
@@ -15,6 +15,12 @@ describe 'homes/show' do
     template.should have_tag('a[href=?]', pitch_path(assigns[:featured_pitch]))
   end
 
+  it "should not raise an exception when there is no featured pitch" do
+    assigns[:featured_pitch] = nil
+    lambda { do_render }.
+      should_not raise_error
+  end
+
   it "should have a field to enter a headline" do
     do_render
     template.should have_tag('textarea[name="headline"]')
