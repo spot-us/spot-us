503634086aefd99ce358d0817f2e3c8320f6f47a
GIT_COMMITTER_NAME <desi@hashrocket.com>
2008-09-25 14:23:25 -0400

    Desi & Tammer: finished pledges dashboard

---
+++ spec/views/myspot/pledges/index.html.haml_spec.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + "/../../../spec_helper"
+
+describe 'myspot/pledges/index' do
+  before do
+    @user = Factory(:user)
+    @tips = [Factory(:tip), Factory(:tip)]
+    @pledges = @tips.collect {|tip| Factory(:pledge, :user => @user, :tip => tip) }
+    assigns[:pledges] = @pledges
+    template.stub!(:current_user).and_return(@user)
+  end
+
+  it 'should render' do
+    do_render
+  end
+
+  def do_render
+    render 'myspot/pledges/index'
+  end
+end
