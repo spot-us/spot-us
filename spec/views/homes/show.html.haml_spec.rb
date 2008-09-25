61a436acb90da5fbf77dc1dee40ea09d188bb2e9
erik <jonlarkowski@gmail.com>
2008-09-25 16:09:27 -0400

    Added the NYT article and 'Make Headlines' form to the home page (+jferris)

--- spec/views/homes/show.html.haml_spec.rb
+++ spec/views/homes/show.html.haml_spec.rb
@@ -15,6 +15,17 @@ describe 'homes/show' do
     template.should have_tag('a[href=?]', pitch_path(assigns[:featured_pitch]))
   end
 
+  it "should have a field to enter a headline" do
+    do_render
+    template.should have_text_field_for('headline')
+  end
+
+  it "should have a form to create a news item" do
+    template.stub!(:start_story_path).and_return('/start/story')
+    do_render
+    template.should have_tag('form[action=?][method="get"]', '/start/story')
+  end
+
   def do_render
     render 'homes/show'
   end
