9fb275feffc99526dd3fe96b14b1a26f4140a321
GIT_AUTHOR_NAME <jonlarkowski@gmail.com>
2008-09-25 12:51:51 -0400

    Fixed donations breaking for guest users (+jferris)

--- spec/controllers/donations_controller_spec.rb
+++ spec/controllers/donations_controller_spec.rb
@@ -1,4 +1,32 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe DonationsController do
+  describe "on POST to create with valid input" do
+    before do
+      login_as @user = Factory(:user)
+      @pitch = Factory(:pitch)
+    end
+    
+    it "should create a valid donation" do
+      do_create
+      assigns[:donation].should_not be_nil
+      assigns[:donation].should be_valid
+    end
+
+    it "should associate donation with current user" do
+      do_create
+      assigns[:donation].user_id.should == @user.id
+    end
+
+    it "should associate donation with the pitch" do
+      do_create
+      assigns[:donation].pitch.should == @pitch
+    end
+
+    def do_create
+      xhr :post, :create, :donation => {:pitch_id => @pitch.id, :amount => 25}
+    end
+  end
+
+  requires_login_for :post, :create
 end
