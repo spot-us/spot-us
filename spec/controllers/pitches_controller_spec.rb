4c015bc4337fee6dff078d1f46fbf952b395f9bd
erik <desi@hashrocket.com>
2008-09-25 18:41:15 -0400

    Desi & Tammer: Adding authorization to controllers

--- spec/controllers/pitches_controller_spec.rb
+++ spec/controllers/pitches_controller_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe PitchesController do
   describe "on GET to /pitchs/new" do
     before(:each) do
+      Pitch.stub!(:createable_by?).and_return(true)
       @user = Factory(:user)
       controller.stub!(:current_user).and_return(@user)
       get :new
@@ -12,10 +13,19 @@ describe PitchesController do
       assigns(:pitch).user_id.should == @user.id
     end
   end
+
+  describe "when can't create on new" do
+    before(:each) do
+      Pitch.stub!(:createable_by?).and_return(false)
+      get :new
+    end
+    it_denies_access
+  end
   
   describe "on GET to /pitches/1/edit" do
     describe "without donations" do
       it "renders edit" do
+        Pitch.stub!(:createable_by?).and_return(true)
         pitch = Factory(:pitch)
         get :edit, :id => pitch.to_param
         response.should render_template(:edit)
@@ -25,6 +35,7 @@ describe PitchesController do
     describe "with donations" do
       it "renders edit" do
         donation = Factory(:donation)
+        Pitch.stub!(:createable_by?).and_return(true)
         get :edit, :id => donation.pitch.to_param
         response.should redirect_to(pitch_url(donation.pitch))
         flash[:error].should match(/cannot edit a pitch that has donations/i)
@@ -32,6 +43,14 @@ describe PitchesController do
     end
   end
 
+  describe "when can't get show" do
+    before(:each) do
+      Pitch.stub!(:createable_by?).and_return(false)
+      get :show, :id => 1
+    end
+    it_denies_access
+  end
+  
   describe "on GET to show" do
     before do
       @pitch = Factory(:pitch)
@@ -43,6 +62,7 @@ describe PitchesController do
     end
 
     def do_show
+      Pitch.stub!(:createable_by?).and_return(true)
       get :show, :id => @pitch.id
     end
   end
@@ -50,6 +70,7 @@ describe PitchesController do
   describe "on GET to new with a headline" do
     before do
       login_as Factory(:user)
+      Pitch.stub!(:createable_by?).and_return(true)
       get :new, :headline => 'example'
     end
 
