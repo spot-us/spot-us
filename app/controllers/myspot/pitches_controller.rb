6dd88aac59c95837437cc4496ad35f0b7e779cac
GIT_COMMITTER_NAME <desi@hashrocket.com>
2008-09-25 21:22:18 -0400

    Tammer and Joe: Fixed all the controllers that Joe forgot

--- app/controllers/myspot/pitches_controller.rb
+++ app/controllers/myspot/pitches_controller.rb
@@ -1,4 +1,10 @@
 class Myspot::PitchesController < ApplicationController
   before_filter :login_required
   resources_controller_for :pitches, :only => :index
+
+  private
+
+  def find_resources
+    current_user.pitches
+  end
 end
