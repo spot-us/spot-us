4e699eaf2136fd20006b5a9b66b720f2d45a62ad
erik <desi@hashrocket.com>
2008-09-25 21:22:18 -0400

    Tammer and Joe: Fixed all the controllers that Joe forgot

--- spec/controllers/myspot/tips_controller_spec.rb
+++ spec/controllers/myspot/tips_controller_spec.rb
@@ -2,5 +2,46 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Myspot::TipsController do
   route_matches("/myspot/tips", :get, :controller => "myspot/tips", :action => "index")
+
+  describe "on GET to index" do
+    before do
+      @user = Factory(:user)
+      @tips = [Factory(:tip)]
+
+      controller.stub!(:current_user).and_return(@user)
+      @user.stub!(:tips).and_return(@tips)
+
+      login_as @user
+    end
+
+    it "should response successfully" do
+      do_index
+      response.should be_success
+    end
+
+    it "should render the index view" do
+      do_index
+      response.should render_template('index')
+    end
+
+    it "should find the current user" do
+      controller.should_receive(:current_user).at_least(1).with().and_return(@user)
+      do_index
+    end
+
+    it "should find the user's tips" do
+      @user.should_receive(:tips).with().and_return(@tips)
+      do_index
+    end
+
+    it "should assign the tips" do
+      do_index
+      assigns[:tips].should == @tips
+    end
+
+    def do_index
+      get :index
+    end
+  end
 end
 
