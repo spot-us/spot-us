a2102438855a33953d8ce7574beff543fedc8c46
erik <jonlarkowski@gmail.com>
2008-09-29 18:36:55 -0400

    configurable smtp, and now caches like dev env for easier debugging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,22 +1,24 @@
-# Settings specified here will take precedence over those in config/environment.rb
+config.action_controller.consider_all_requests_local = true
+config.action_controller.perform_caching = false
+config.action_mailer.raise_delivery_errors = true
+config.action_view.debug_rjs = true
+config.cache_classes = false
+config.whiny_nils = true
 
-# The production environment is meant for finished, "live" apps.
-# Code is not reloaded between requests
-config.cache_classes = true
+config.to_prepare do
+  if APP_CONFIG['action_mailer'].is_a?(Hash)
+    config.action_mailer.delivery_method = APP_CONFIG['action_mailer']['delivery_method']
+    config.action_mailer.smtp_settings   = APP_CONFIG['action_mailer']['smtp_settings']
+  end
 
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
-
-# Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
-config.action_view.cache_template_loading            = true
-
-# Use a different cache store in production
-# config.cache_store = :mem_cache_store
-
-# Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host                  = "http://assets.example.com"
-
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+  if APP_CONFIG['gateway'].is_a?(Hash)
+    Purchase.gateway = ActiveMerchant::Billing::AuthorizeNetGateway.new({
+      :login    => APP_CONFIG['gateway']['login'],
+      :password => APP_CONFIG['gateway']['password'],
+      :test => true
+    })
+  else
+    ActiveMerchant::Billing::Base.mode = :test
+    Purchase.gateway = ActiveMerchant::Billing::BogusGateway.new
+  end
+end
