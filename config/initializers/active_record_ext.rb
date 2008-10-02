f84c266108c65cd0c4868c5b46cc095a0cb4b019
GIT_COMMITTER_NAME <jonlarkowski@gmail.com>
2008-10-02 11:48:05 -0400

    added db population rake task and seed data for topics

---
+++ config/initializers/active_record_ext.rb
@@ -0,0 +1,22 @@
+class ActiveRecord::Base
+  # given a hash of attributes including the ID, look up the record by ID. 
+  # If it does not exist, it is created with the rest of the options. 
+  # If it exists, it is updated with the given options. 
+  #
+  # Raises an exception if the record is invalid to ensure seed data is loaded correctly.
+  # 
+  # Returns the record.
+  #
+  # http://railspikes.com/2008/2/1/loading-seed-data
+  def self.create_or_update(options = {})
+    id = options.delete(:id)
+    record = find_by_id(id) || new
+    record.id = id
+    
+    # we have to override any attribute protections by going to 'send' directly
+    options.each { |k, v| record.send("#{k}=", v) }
+    
+    record.save!
+    record
+  end
+end
