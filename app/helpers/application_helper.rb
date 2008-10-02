1855e25d749dc0a24e5f081538bcef66f7bac82c
GIT_AUTHOR_NAME <jonlarkowski@gmail.com>
2008-10-02 17:42:21 -0400

    Desi and Lark: finishing the topic picker

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,12 +5,12 @@ module ApplicationHelper
     controller.controller_path.underscore.gsub('/', '_')
   end
 
-  def topic_check_boxes(resource)
-    render :partial => Topic.all, :locals => {:resource => resource}
+  def topic_check_boxes(resource, model = nil)
+    render :partial => "topics/topic", :collection => Topic.all, :locals => {:resource => resource, :model => model}
   end
   
   def show_topics(resource)
-    render :partial => "topics/show_topic", :collection => resource.topics
+    render :inline => resource.topics.map(&:name).join(', ')
   end
 
 end
