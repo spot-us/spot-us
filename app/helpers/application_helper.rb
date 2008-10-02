39ad75e4f921d4829177f549813215a863acab62
erik <jonlarkowski@gmail.com>
2008-10-02 14:43:28 -0400

    Desi and Lark: working on topic picking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     controller.controller_path.underscore.gsub('/', '_')
   end
 
+  def topic_check_boxes(resource)
+    render :partial => Topic.all, :locals => {:resource => resource}
+  end
+  
+  def show_topics(resource)
+    render :partial => "topics/show_topic", :collection => resource.topics
+  end
+
 end
--- spec/views/pitches/new.html.haml_spec.rb
+++ spec/views/pitches/new.html.haml_spec.rb
@@ -2,6 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/pitches/new.html.haml" do
   include PitchesHelper
+  include ApplicationHelper
   
   before(:each) do
     @pitch = stub_model(Pitch)
