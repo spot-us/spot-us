351f1f3b6b637d170ebeb32ab8a01bf5b0932684
erik <jonlarkowski@gmail.com>
2008-10-03 15:17:27 -0400

    added handy testing users.  had to remove some meta-magick that was killing rake db:populate

---
+++ db/fixtures/shared/users.rb
@@ -0,0 +1,36 @@
+unless Rails.env.production?
+  testing_users = [
+    %w(David Cohn david@spot.us),
+    %w(Desi McAdam desi@hashrocket.com),
+    %w(Jon Larkowski lark@hashrocket.com)
+  ]
+
+  testing_users.each_with_index do |u, i|
+    user_hash = {
+      :first_name => u[0],
+      :last_name => u[1],
+      :password => '12345',
+      :password_confirmation => '12345',
+    }
+  
+    original_email = u[2]
+  
+    # makes three types of users for each tester
+    # example:  lark@hashrocket.com
+    #           lark+citizen@hashrocket.com
+    #           lark+reporter@hashrocket.com
+    #           lark+organization@hashrocket.com  
+  
+    user_hash[:id] = 3*i + 1
+    user_hash[:email] = original_email.gsub /@/, "+citizen@"
+    Citizen.create_or_update user_hash
+
+    user_hash[:id] = 3*i + 2
+    user_hash[:email] = original_email.gsub /@/, "+reporter@"
+    Reporter.create_or_update user_hash
+  
+    user_hash[:id] = 3*i + 3
+    user_hash[:email] = original_email.gsub /@/, "+organization@"
+    Organization.create_or_update user_hash
+  end
+end
