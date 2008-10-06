2050be0e509bee08781a4c7ce3d8b55f2adee570
GIT_AUTHOR_NAME <dev+jpl+trp@hashrocket.com>
2008-10-06 10:26:09 -0400

    missing route test change

--- spec/controllers/myspot/jobs_controller_spec.rb
+++ spec/controllers/myspot/jobs_controller_spec.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Myspot::JobsController do
-  route_matches("/myspot/jobs/1/edit", :get, :controller => "myspot/jobs", :action => "edit", :id => "1")
+  route_matches("/myspot/profile/jobs/1/edit", :get, :controller => "myspot/jobs", :action => "edit", :id => "1")
 end
