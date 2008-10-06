b8cb8f4deb4ebf30ec7c379423df7a9d358326ae
GIT_AUTHOR_NAME <jonlarkowski@gmail.com>
2008-10-06 11:01:01 -0400

    updated pitch accepted content, tweaked configs, now using url writer

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,43 +1,46 @@
 class Mailer < ActionMailer::Base
+  include ActionController::UrlWriter 
+  default_url_options[:host] = DEFAULT_HOST
+  
   def citizen_signup_notification(user)
     recipients user.email
-    from       '"Spot.Us" <donotreply@spot.us>'
-    subject    "Welcome to Spot.Us – “Community Funded Reporting.”"
+    from       MAIL_FROM_INFO
+    subject    %(Welcome to Spot.Us – "Community Funded Reporting")
     body :user => user
   end
   
   def reporter_signup_notification(user)
     recipients user.email
-    from       '"Spot.Us" <donotreply@spot.us>'
+    from       MAIL_FROM_INFO
     subject    "Welcome to Spot.Us – Reporting on Communities"
     body :user => user
   end
   
   def organization_signup_notification(user)
     recipients user.email
-    from       '"Spot.Us" <donotreply@spot.us>'
+    from       MAIL_FROM_INFO
     subject    "Spot.Us: Important Information on Joining"
     body :user => user
   end
   
   def news_org_signup_request(user)
     recipients '"David Cohn" <david@spotus.com>'
-    from       '"Spot.Us" <donotreply@spot.us>'
+    from       MAIL_FROM_INFO
     subject    "Spot.Us: News Org Requesting to Join"
     body        :user => user
   end
 
   def password_reset_notification(user)
     recipients user.email
-    from       '"Spot.Us" <donotreply@spot.us>'
-    subject    "Password Reset"
+    from       MAIL_FROM_INFO
+    subject    "Spot.Us: Password Reset"
     body       :user => user
   end
   
   def pitch_accepted_notification(pitch)
     recipients pitch.supporters.map(&:email).join(', ')
-    from       '"Spot.Us" <donotreply@spot.us>'
-    subject    "Pitch: Has become an assignment!"
+    from       MAIL_FROM_INFO
+    subject    "Spot.Us: Success!! Your Story is Funded!"
     body       :pitch => pitch
   end
 end
