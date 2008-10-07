4b97dadef7e1034b2c7872c2d5f71fed924bf6c5
GIT_AUTHOR_NAME <dev+desi+l4rk@hashrocket.com>
2008-10-07 14:07:28 -0400

    working on dynamic header for current balance

--- spec/views/layouts/application.html.haml_spec.rb
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -2,8 +2,25 @@ require File.dirname(__FILE__) + "/../../spec_helper"
 
 describe 'layouts/application' do
   it 'should render' do
-    
-        
     render 'layouts/application'
+  end
+  
+  describe "current balance" do
+    before do
+      @user = Factory(:user)
+      template.stub!(:current_user).and_return(@user)
+      template.stub!(:logged_in?).and_return(true)
+    end
+    
+    it "shows no notice when no balance" do
+      render 'layouts/application'
+      response.should_not have_tag("#current_balance")
+    end
+    
+    it "shows notice when there's a balance" do
+      donations = Factory(:donation, :user => @user)
+      render 'layouts/application'
+      response.should have_tag("#current_balance")
+    end    
   end
 end
