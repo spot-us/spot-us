3992a85c860c037499680b4412f5d2e105fb5470
Jon Larkowski <dev+desi+l4rk@hashrocket.com>
2008-10-08 17:34:54 -0400

    adding ability to impersonate a user as an admin

---
+++ vendor/plugins/pacecar/test/order_test.rb
@@ -0,0 +1,28 @@
+require File.join(File.dirname(__FILE__), 'test_helper')
+
+class OrderTest < Test::Unit::TestCase
+
+  context "A class which has included Pacecar" do
+    setup do
+      @class = User
+    end
+    context "with order methods" do
+      should "set the correct proxy options for a by_ column method with no args" do
+        assert @class.respond_to? :by_first_name
+        proxy_options = { :order => '"users".first_name asc' }
+        assert_equal proxy_options, @class.by_first_name.proxy_options
+      end
+      should "set the correct proxy options for a by_ column method with asc args" do
+        assert @class.respond_to? :by_first_name
+        proxy_options = { :order => '"users".first_name asc' }
+        assert_equal proxy_options, @class.by_first_name(:asc).proxy_options
+      end
+      should "set the correct proxy options for a by_ column method with desc args" do
+        assert @class.respond_to? :by_first_name
+        proxy_options = { :order => '"users".first_name desc' }
+        assert_equal proxy_options, @class.by_first_name(:desc).proxy_options
+      end
+    end
+  end
+
+end
