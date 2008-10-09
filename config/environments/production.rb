64bd3ab856e2288850389532575f8eb3f5490bfd
erik <jonlarkowski@gmail.com>
2008-10-09 15:37:30 -0400

    setting up prod env config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,22 +1,19 @@
-# Settings specified here will take precedence over those in config/environment.rb
-
-# The production environment is meant for finished, "live" apps.
-# Code is not reloaded between requests
-config.cache_classes = true
-
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
-
-# Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
-config.action_view.cache_template_loading            = true
+config.action_controller.perform_caching = true
+config.action_mailer.raise_delivery_errors = false
+config.action_view.cache_template_loading = true
+config.action_view.debug_rjs = false
+config.cache_classes = true
+config.whiny_nils = true
 
-# Use a different cache store in production
-# config.cache_store = :mem_cache_store
+if APP_CONFIG[:action_mailer].is_a?(Hash)
+  config.action_mailer.delivery_method = APP_CONFIG[:action_mailer][:delivery_method]
+  config.action_mailer.smtp_settings   = APP_CONFIG[:action_mailer][:smtp_settings]
+end
 
-# Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host                  = "http://assets.example.com"
-
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+config.after_initialize do
+  Purchase.gateway = ActiveMerchant::Billing::AuthorizeNetGateway.new({
+    :login    => APP_CONFIG[:gateway][:login],
+    :password => APP_CONFIG[:gateway][:password],
+  })
+end
