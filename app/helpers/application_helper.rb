77ca80b4c3db3fac39f939815eefd77b43cd3863
GIT_AUTHOR_NAME <carmelyne@hashrocket.com>
2008-10-09 13:22:26 -0500

    ui -- styling for the summary section of news items on home page, truncating summary to 30 words

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,7 @@ module ApplicationHelper
     render :inline => resource.topics.map(&:name).join(', ')
   end
   
-  def truncate_words(text, length = 30, end_string = ' …')
+  def truncate_words(text, length = 30, end_string = '… ')
     words = text.split()
     words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
   end
