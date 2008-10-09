2e3e0fb9efdce8f86338a21262a4716aa3053edb
GIT_AUTHOR_NAME <jonlarkowski@gmail.com>
2008-10-09 15:37:45 -0400

    tweaking staging env config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,6 +2,7 @@ config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching = false
 config.action_mailer.raise_delivery_errors = true
 config.action_view.debug_rjs = true
+config.action_view.cache_template_loading = false
 config.cache_classes = false
 config.whiny_nils = true
 
