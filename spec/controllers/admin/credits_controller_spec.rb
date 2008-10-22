f5d5e003f5ca873171baac50404525f06561755e
erik <desi.mcadam@gmail.com>
2008-10-22 13:51:33 -0400

    Desi - fixing a broken test I checked in by accident

--- spec/controllers/admin/credits_controller_spec.rb
+++ spec/controllers/admin/credits_controller_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 describe Admin::CreditsController do
   describe "get to index" do
     it "should supply list of credits in descending order" do
+      controller.should_receive(:admin_required).and_return(true)
       get :index
       assigns[:users].should_not be_nil
       assigns[:credits].should_not be_nil
