34c0bebac5da4f65ced4bbf49b0126d4136d8bb6
GIT_AUTHOR_NAME <desi.mcadam@gmail.com>
2008-10-22 13:45:40 -0400

    Desi and Obie - working on credits work

--- app/controllers/admin/credits_controller.rb
+++ app/controllers/admin/credits_controller.rb
@@ -1,4 +1,7 @@
 class Admin::CreditsController < ApplicationController
+  before_filter :admin_required
+  layout "bare"
+  
   def index
     @credits = Credit.find :all, :order => 'created_at desc'
   end
