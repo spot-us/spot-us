6e0e4d25707e2011362957805b3c5f1b920641e4
Desi McAdam a.k.a The Chick <desi.mcadam@gmail.com>
2008-10-22 20:57:48 -0400

    Desi and Obie - finishing up story 255603

--- spec/controllers/admin/credits_controller_spec.rb
+++ spec/controllers/admin/credits_controller_spec.rb
@@ -1,12 +1,56 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Admin::CreditsController do
-  describe "get to index" do
+  describe "GET to index" do
     it "should supply list of credits in descending order" do
       controller.should_receive(:admin_required).and_return(true)
       get :index
-      assigns[:users].should_not be_nil
       assigns[:credits].should_not be_nil
+    end
+    
+    it "should supply a blank credit for the add credit form" do
+      controller.should_receive(:admin_required).and_return(true)
+      get :index
+      assigns[:credit].should_not be_nil
+    end
+    
+    it "should supply list of users for the add credit form" do
+      controller.should_receive(:admin_required).and_return(true)
+      get :index
+      assigns[:users].should_not be_nil
+    end
+  end
+  
+  describe "POST to create" do
+    before(:each) do
+      login_as Factory(:admin)
+      @user = Factory(:user)
+    end
+    
+    it "should create a new credit" do
+      @user.credits.should be_empty
+      do_post
+      @user.reload
+      @user.credits.first.amount.should == "35.0"
+      @user.credits.first.description.should == "Fatass Sympathy"
+    end
+    
+    it "should redirect to index on successful post" do
+      do_post
+      response.should redirect_to(admin_credits_path)
+    end
+    
+    it "should not redirect on invalid data" do
+      post :create
+      response.should render_template("admin/credits/index")
+      assigns[:credit].errors.should_not be_empty
+    end
+    
+    def do_post
+      post :create, 
+           :credit => { :amount => "35",
+                        :description => "Fatass Sympathy",
+                        :user_id => @user.id }
     end
   end
 end
--- spec/views/admin/credits/index.html.haml_spec.rb
+++ spec/views/admin/credits/index.html.haml_spec.rb
@@ -4,8 +4,11 @@ describe '/admin/credits/index' do
   it "should render" do
     user = Factory(:admin)
     template.stub!(:current_user).and_return(@user)
-    credits = [Factory(:credit), Factory(:credit)]
+    users = [Factory(:user), Factory(:user)]
+    credits = [Factory(:credit, :user => users[0]), Factory(:credit, :user => users[1])]
+    assigns[:credit] = Credit.new
     assigns[:credits] = credits
+    assigns[:users] = users
     render '/admin/credits/index'
   end
 end
