048a603b60804173b31addb1d841a46f1b59c0c2
erik <desi.mcadam@gmail.com>
2008-11-03 13:35:23 -0500

    Desi and Steve - making the browse stuff work

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -1,6 +1,11 @@
 class NewsItemsController < ApplicationController  
+  include NewsItemsHelper
   def index
     get_news_items
+  end
+  
+  def sort_options
+    render :text => options_for_sorting(params.fetch(:news_item_type, "news_items"), params.fetch(:sort_by,"desc"))
   end
   
   def search
@@ -9,18 +14,16 @@ class NewsItemsController < ApplicationController
   end
   
   protected
-  def get_news_items
-    unless params[:news_item_types].blank? 
-      @news_items = NewsItem.find :all,
-                    :order => "created_at #{params.fetch(:date_sort, 'desc')}",
-                    :conditions => {
-                      :type => params[:news_item_types].symbolize_keys!.collect do |item, value| 
-                        item.to_s.capitalize
-                      end
-                      }
+  def get_news_items   
+    unless params[:news_item_type].blank?
+      params[:sort_by] = 'desc' unless %w(desc asc most_pledged most_funded almost_funded).include?(params[:sort_by])
+      @news_items = params[:news_item_type].camelize.singularize.constantize.send(params[:sort_by])
     else
       @news_items = NewsItem.find :all,
-                    :order => "created_at #{params.fetch(:date_sort, 'desc')}"
+                    :order => "created_at #{params.fetch(:sort_by, 'desc')}"
     end
   end
+  
+  
+  
 end
