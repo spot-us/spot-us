52a6277f04289638a6f0833136c31cf068b711e3
GIT_AUTHOR_NAME <desi.mcadam@gmail.com>
2008-11-04 10:58:04 -0500

    Desi and Stephen - fully funded donations and accepted donations progress bar and pitch update to reflect fully funded

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -34,6 +34,14 @@
 
 class NewsItem < ActiveRecord::Base
   include HasTopics
+  include AASM
+  class <<self
+    alias invasive_inherited_from_aasm inherited
+    def inherited(child)
+      invasive_inherited_from_aasm(child)
+      super
+    end
+  end
   belongs_to :user
   
   has_attached_file :featured_image, 
