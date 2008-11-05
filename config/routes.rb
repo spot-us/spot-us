c63a0ebdd3fe82d5b37efcd122fe9f3a2618f3da
GIT_AUTHOR_NAME <desi.mcadam@gmail.com>
2008-11-05 16:01:07 -0500

    Stephen - Allow assigning fact checkers for a Pitch

--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,8 @@ ActionController::Routing::Routes.draw do |map|
   map.namespace :admin do |admin|
     admin.resources :users, :member => {:log_in_as => :get, :approve => :put}
     admin.resources :credits 
-    admin.resources :pitches 
-    admin.resources :tips 
+    admin.resources :pitches, :member => { :fact_checker_chooser => :get }
+    admin.resources :tips
   end
 
   map.namespace :myspot do |myspot|
---
+++ public/javascripts/admin.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($){
+  function get_fact_check_chooser(anchor) {
+    var td = $(anchor).parent('td.fact_checker');
+    var chooser = $.get($(anchor).attr('href'), {}, 
+      function(html, status){ td.html(html); }
+    );
+  }
+  $('td.fact_checker a.change_fact_checker').click(function(e){
+    get_fact_check_chooser(this);
+    return false;
+  });
+});
