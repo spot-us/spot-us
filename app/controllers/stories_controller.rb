97388e7bcab9e3bf5a9a08d21d5e1e6bd7e1144d
GIT_COMMITTER_NAME <desi.mcadam@gmail.com>
2008-11-07 12:47:58 -0500

    Desi - WIP - acl for story

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,3 +1,28 @@
 class StoriesController < ApplicationController
+  before_filter :can_view?, :only => [:show]  
   resources_controller_for :stories
+  
+  protected
+    def can_view?
+      story = find_resource
+      unless story.viewable_by?(current_user)
+        access_denied( \
+          :flash => "This story is not viewable at this time.",
+          :redirect => pitch_url(story.pitch))
+      end
+    end
+    
+    def can_edit?
+      story = find_resource
+      if current_user.nil?
+        access_denied( \
+          :flash => "You cannot edit a story unless you are logged in.", 
+          :redirect => new_session_path)
+      end
+      unless story.editable_by?(current_user)
+        access_denied( \
+          :flash => "You cannot edit this story, contact info@spot.us", 
+          :redirect => story_url(story))
+      end
+    end
 end
