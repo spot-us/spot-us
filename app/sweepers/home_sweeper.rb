803a6782bf04732b5392b7f7b0ff75473c86d98f
erik <desi.mcadam@gmail.com>
2008-11-07 17:36:09 -0500

    Desi - fixing the sweeper so it works with namespaces

--- app/sweepers/home_sweeper.rb
+++ app/sweepers/home_sweeper.rb
@@ -2,11 +2,11 @@ class HomeSweeper < ActionController::Caching::Sweeper
   observe NewsItem
   
   def after_save(news_item)
-    expire_page(:controller => 'homes', :action => 'show')
+    expire_page(:controller => '/homes', :action => 'show')
   end
   
   def after_destroy(news_item)
-    expire_page(:controller => 'homes', :action => 'show')
+    expire_page(:controller => '/homes', :action => 'show')
   end
 
 end
