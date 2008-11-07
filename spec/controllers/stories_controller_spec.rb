1bf5e20e96aacb31dea97b9c4c6f84a5c7c0740a
GIT_AUTHOR_NAME <desi.mcadam@gmail.com>
2008-11-07 12:47:58 -0500

    Desi - WIP - acl for story

--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -13,5 +13,23 @@ describe StoriesController do
   route_matches('/stories/1', :get, :controller => 'stories',
                                           :action     => 'show',
                                           :id => "1")
+                                          
+  describe "can_edit?" do
+    it "should allow owner to edit if in draft state" do
+      user = Factory(:user)
+      story = Factory(:story, :user => user)
+      controller.stub!(:current_user).and_return(user)
+      get :edit, :id => story.id 
+    end
+    
+    it "should not allow the owner to edit if it is in any state other than draft" do
+      user = Factory(:user)
+      story = Factory(:story, :user => user, :status => "accepted")
+      controller.stub!(:current_user).and_return(user)
+      get :edit, :id => story.id
+      flash[:error].should_not be_nil
+    end
+    
+  end
                                                
 end
