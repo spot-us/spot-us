4fd1ef44828f37e5b145515491d9ad6803a3cdfe
GIT_AUTHOR_NAME <obie@hashrocket.com>
2008-11-07 16:09:17 -0500

    turned caching on staging so we can test the caching stuff we just added

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,5 @@
 config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching = false
+config.action_controller.perform_caching = true
 config.action_mailer.raise_delivery_errors = true
 config.action_view.debug_rjs = true
 config.action_view.cache_template_loading = false
