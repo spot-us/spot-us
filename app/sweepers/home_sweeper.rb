fbf77c25f6f7d217bc548fe4083f8227bc8d1ed9
GIT_COMMITTER_NAME <obie@hashrocket.com>
2008-11-07 18:10:05 -0500

    update_balance in the header on login/logout and donation changes

--- app/sweepers/home_sweeper.rb
+++ app/sweepers/home_sweeper.rb
@@ -2,11 +2,18 @@ class HomeSweeper < ActionController::Caching::Sweeper
   observe NewsItem
   
   def after_save(news_item)
-    expire_page(:controller => '/homes', :action => 'show')
+    expire_pages
   end
   
   def after_destroy(news_item)
-    expire_page(:controller => '/homes', :action => 'show')
+    expire_pages
   end
+  
+  private
+    
+    def expire_pages
+      expire_page(:controller => 'homes', :action => 'show')
+      expire_page(:controller => 'news_items', :action => 'index')
+    end
 
 end
