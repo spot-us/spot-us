3bb918d900faa09268a982005bf83cf577e0d182
erik <jonlarkowski@gmail.com>
2008-11-07 14:07:04 -0500

    now populates sample users by email address to avoid prod data collision

--- db/fixtures/shared/users.rb
+++ db/fixtures/shared/users.rb
@@ -22,24 +22,20 @@ unless Rails.env.production?
     #           lark+reporter@hashrocket.com
     #           lark+organization@hashrocket.com  
   
-    user_hash[:id] = 4*i + 1
     user_hash[:email] = original_email.gsub /@/, "+citizen@"
     user_hash[:type] = Citizen.name
-    User.create_or_update user_hash
+    User.create_or_update_by_email user_hash
     
-    user_hash[:id] = 4*i + 2
     user_hash[:email] = original_email.gsub /@/, "+reporter@"
     user_hash[:type] = Reporter.name
-    User.create_or_update user_hash
+    User.create_or_update_by_email user_hash
   
-    user_hash[:id] = 4*i + 3
     user_hash[:email] = original_email.gsub /@/, "+organization@"
     user_hash[:type] = Organization.name
-    User.create_or_update user_hash
+    User.create_or_update_by_email user_hash
 
-    user_hash[:id] = 4*i + 4
     user_hash[:email] = original_email.gsub /@/, "+admin@"
     user_hash[:type] = Admin.name
-    User.create_or_update user_hash
+    User.create_or_update_by_email user_hash
   end
 end
