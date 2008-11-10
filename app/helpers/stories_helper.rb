2a725a59ea204b4d21e448c3321decd1f8d4a0e2
GIT_AUTHOR_NAME <desi.mcadam@gmail.com>
2008-11-10 17:53:10 -0500

    Desi and Stephen - finishing up the story stuff

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -2,4 +2,27 @@ module StoriesHelper
   def supporters_count
     @story.pitch.supporters.size
   end
+  
+  def publishing_workflow_buttons_for(user)
+    out = "<div class='centered'>"
+    if (@story.draft? && @story.editable_by?(user)) || user.admin?
+      out << content_tag(:a, tag(:img, :class => 'edit'), :href => '')
+    end
+    case @story.status
+    when 'draft' then
+      if @story.editable_by?(user)
+        out << content_tag(:a, tag(:img, :class => 'send_to_editor'), :href => '')
+      end
+    when 'fact_check' then
+      if @story.fact_checkable_by?(user)
+        out << content_tag(:a, tag(:img, :class => 'return_to_journalist'), :href => '')
+        out << content_tag(:a, tag(:img, :class => 'ready_for_publishing'), :href => '')
+      end      
+    when 'ready' then
+      if @story.publishable_by?(user)
+        out << content_tag(:a, tag(:img, :class => 'publish'), :href => '')
+      end
+    end
+    out << "</div>"
+  end
 end
