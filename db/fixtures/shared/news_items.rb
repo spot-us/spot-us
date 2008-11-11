753b6aaa76e9bda37cc49bf20476e4fe82296004
GIT_COMMITTER_NAME <dungtn@gmail.com>
2008-11-12 04:21:47 +0800

    refactor news_items fixture

--- db/fixtures/shared/news_items.rb
+++ db/fixtures/shared/news_items.rb
@@ -1,25 +1,28 @@
 unless Rails.env.production?
-  count = 0
-  10.times do
-    count = count + 1
+  1.upto 22 do |count|
     Factory(:tip, :headline => "Tip Headline #{count}")
   end
+
   tips = Tip.find :all
+
   tips.each do |tip| 
     2.times do
-    Factory(:pledge, :tip => tip, :amount => 25)
+      Factory(:pledge, :tip => tip, :amount => 25)
     end
   end
-  count = 0
-  10.times do 
-    count = count + 1
+
+
+  1.upto 22 do |count|
     Factory(:pitch, :headline => "Pitch Headline #{count}")
   end
+
   pitches = Pitch.find :all
+
   pitches.each do |pitch| 
     2.times do 
       Factory(:donation, :pitch => pitch, :amount => 10, :status => 'paid')
     end
+
     2.times do 
       Factory(:donation, :pitch => pitch, :amount => 10)
     end
