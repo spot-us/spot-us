a7d061fabfb878a250d6b08ddab10726353826ac
GIT_COMMITTER_NAME <desi.mcadam@gmail.com>
2008-11-11 12:11:47 -0500

    Desi - fixing bad link to engineyard

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,5 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
-
+# rspec is not playing nice
+require File.expand_path(File.join %W(#{RAILS_ROOT} app controllers admin users_controller))
 describe Admin::UsersController do
   describe "get to index" do
     it "should respond to csv" do
