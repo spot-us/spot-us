39e62782d6d3f9cbc8de201e66fe65a1391654b1
erik <dungtn@gmail.com>
2008-11-12 10:04:18 +0800

    easier to read

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -6,22 +6,29 @@ class NewsItemsController < ApplicationController
   end
   
   def sort_options
-    render :text => options_for_sorting(params.fetch(:news_item_type, "news_items"), params.fetch(:sort_by,"desc"))
+    render :text => options_for_sorting(
+      params.fetch(:news_item_type, "news_items"), 
+      params.fetch(:sort_by, "desc")
+    )
   end
   
   def search
     get_news_items
-    render :action => 'index'    
+    render :action => 'index'
   end
   
   protected
-  def get_news_items   
+  def get_news_items
     unless params[:news_item_type].blank?
       params[:sort_by] = 'desc' unless %w(desc asc most_pledged most_funded almost_funded).include?(params[:sort_by])
-      @news_items = params[:news_item_type].camelize.singularize.constantize.send(params[:sort_by]).paginate(:all, :page => params[:page])
+      klass = params[:news_item_type].camelize.singularize.constantize
+      @news_items = klass.send(params[:sort_by]).
+                          paginate(:all, :page => params[:page])
     else
-      @news_items = NewsItem.paginate :all, :page => params[:page],
-                    :order => "created_at #{params.fetch(:sort_by, 'desc')}", :conditions => ["type in (?)", ['Pitch', 'Tip']]
+      @news_items = NewsItem.paginate :all, 
+                      :page => params[:page],
+                      :order => "created_at #{params.fetch(:sort_by, 'desc')}", 
+                      :conditions => ["type in (?)", ['Pitch', 'Tip']]
     end
   end
 end
