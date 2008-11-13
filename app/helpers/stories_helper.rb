7a69a95ad5de52f145adf1c515a656368ef8556d
Stephen Caudill <vox@exdolo.com>
2008-11-13 14:21:20 -0500

    Stephen - updated stories edit button image

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -2,11 +2,11 @@ module StoriesHelper
   def supporters_count
     @story.pitch.supporters.size
   end
-  
+
   def publishing_workflow_buttons_for(user)
     out = ""
     if (@story.draft? && @story.editable_by?(user)) || user.admin?
-      out << content_tag(:div, link_to(image_tag('edit.png', :class => 'edit'), edit_story_path(@story)), :class => 'centered')
+      out << content_tag(:div, link_to(image_tag('edit_in_gray.png', :class => 'edit'), edit_story_path(@story)), :class => 'centered')
     end
     case @story.status
     when 'draft' then
@@ -20,7 +20,7 @@ module StoriesHelper
                                 reject_story_path(@story), :method => :put), :class => 'centered')
         out << content_tag(:div, link_to(image_tag('ready_for_publishing.png', :class => 'ready_for_publishing'), 
                                   accept_story_path(@story), :method => :put), :class => 'centered')
-      end      
+      end
     when 'ready' then
       if @story.publishable_by?(user)
         out << content_tag(:div, link_to(image_tag('publish.png', :class => 'publish'), 
