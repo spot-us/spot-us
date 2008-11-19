178c694616812650213d45456f5353cdd33cdcad
GIT_AUTHOR_NAME <desi.mcadam@gmail.com>
2008-11-19 16:35:23 -0500

    Desi - fixing bug around start a story going to the right place when user wasn't logged in

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,5 +4,16 @@ class HomesController < ApplicationController
   def show
     @featured_pitch = Pitch.featured.first
   end
+  
+  def start_story
+    if !logged_in?
+      session[:return_to] = start_story_path
+      redirect_to new_session_path
+    elsif logged_in? && current_user.is_a?(Reporter)
+      redirect_to new_pitch_path
+    else
+      redirect_to new_tip_path
+    end
+  end
 
 end
