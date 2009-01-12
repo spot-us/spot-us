b1f53ec1fc974464a561bbf5327a4903021e7b86
Stephen Caudill <vox@exdolo.com>
2009-01-12 11:48:14 -0500

    Update monkey patch to work with AASM 2.0.5

--- lib/aasm_with_fixes.rb
+++ lib/aasm_with_fixes.rb
@@ -1,14 +1,16 @@
+module AASM
+  class StateMachine
+    def clone
+      klone = super
+      klone.states = states.clone
+      klone.events = events.clone
+      klone
+    end
+  end
+end
+
 module AASMWithFixes
   def self.included(base)
     base.send(:include, AASM)
-    base.module_eval do
-      class << self
-        def inherited(child)
-          AASM::StateMachine[child] = AASM::StateMachine[self].clone
-          AASM::StateMachine[child].events = AASM::StateMachine[self].events.clone
-          super
-        end
-      end
-    end
   end
 end
