3da116ed711336679ae50adac65a86ec575f946d
GIT_AUTHOR_NAME <vox@exdolo.com>
2009-01-12 15:27:20 -0500

    Updated RSpec to the most recent sane version

--- vendor/plugins/rspec-rails/lib/spec/rails/example/helper_example_group.rb
+++ vendor/plugins/rspec-rails/lib/spec/rails/example/helper_example_group.rb
@@ -30,6 +30,29 @@ module Spec
           def protect_against_forgery?
             false
           end
+          
+          def session=(session)
+            @session = session
+          end
+          
+          def request=(request)
+            @request = request
+          end
+          
+          def flash=(flash)
+            @flash = flash
+          end
+          
+          def params=(params)
+            @params = params
+          end
+          
+          def controller=(controller)
+            @controller = controller
+          end
+          
+          private
+            attr_reader :session, :request, :flash, :params, :controller
         end
         
         class << self
@@ -73,7 +96,11 @@ module Spec
         def helper
           self.class.helper
         end
-
+        
+        def orig_assigns
+          helper.assigns
+        end
+        
         # Reverse the load order so that custom helpers which are defined last
         # are also loaded last.
         ActionView::Base.included_modules.reverse.each do |mod|
@@ -81,7 +108,7 @@ module Spec
         end
 
         before(:all) do
-          @controller_class_name = 'Spec::Rails::Example::HelperBehaviourController'
+          @controller_class_name = 'Spec::Rails::Example::HelperExampleGroupController'
         end
 
         before(:each) do
@@ -92,6 +119,12 @@ module Spec
           session['flash'] = @flash
 
           ActionView::Helpers::AssetTagHelper::reset_javascript_include_default
+          
+          helper.session = session
+          helper.request = @request
+          helper.flash = flash
+          helper.params = params
+          helper.controller = @controller
         end
 
         def flash
@@ -99,8 +132,13 @@ module Spec
         end
 
         def eval_erb(text)
+          erb_args = [text]
+          if helper.respond_to?(:output_buffer)
+            erb_args += [nil, nil, '@output_buffer']
+          end
+          
           helper.instance_eval do
-            ERB.new(text).result(binding)
+            ERB.new(*erb_args).result(binding)
           end
         end
 
@@ -114,12 +152,14 @@ module Spec
 
         protected
         def _assigns_hash_proxy
-          @_assigns_hash_proxy ||= AssignsHashProxy.new helper
+          @_assigns_hash_proxy ||= AssignsHashProxy.new self do
+            helper
+          end
         end
 
       end
 
-      class HelperBehaviourController < ApplicationController #:nodoc:
+      class HelperExampleGroupController < ApplicationController #:nodoc:
         attr_accessor :request, :url
 
         # Re-raise errors
