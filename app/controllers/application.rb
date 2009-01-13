eee9696480078d3c12a2fa9e73b3d95a0eb3fa5c
Jacqui Maher and Stephen Caudill <dev+jacqui+voxdolo@hashrocket.com>
2009-01-13 11:51:31 -0500

    Set the balance text when logging in from cookie

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,20 +2,21 @@ class ApplicationController < ActionController::Base
   include HoptoadNotifier::Catcher
   filter_parameter_logging :password, :password_confirmation, :credit_card_number
   helper :all # include all helpers, all the time
+  include ApplicationHelper
   include AuthenticatedSystem
   include SslRequirement
-  
+
   # cache_sweeper :home_sweeper, :except => [:index, :show]
 
   before_filter :can_create?, :only => [:new, :create]
   before_filter :can_edit?, :only => [:edit, :update, :destroy]
-  
+
   map_resource :profile, :singleton => true, :class => "User", :find => :current_user
 
   protect_from_forgery
-  
+
   protected
-  
+
     def create_current_login_cookie
       cookies[:current_user_full_name] = current_user.full_name
     end
@@ -27,10 +28,9 @@ class ApplicationController < ActionController::Base
     def can_edit?
       true
     end
-    
+
     def update_balance_cookie
-      return if not logged_in?    
-      cookies[:balance_text] = render_to_string(:partial => 'layouts/balance')
+      cookies[:balance_text] = header_display_message
     end
-  
+
 end
