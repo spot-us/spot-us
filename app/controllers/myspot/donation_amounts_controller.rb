84318d540f973367a36569986db3658003fd2774
erik <dev+jacqui+voxdolo@hashrocket.com>
2009-01-20 16:00:20 -0500

    Added ability to make donations to Support Spot.Us
    resolves story #312890

--- app/controllers/myspot/donation_amounts_controller.rb
+++ app/controllers/myspot/donation_amounts_controller.rb
@@ -5,17 +5,24 @@ class Myspot::DonationAmountsController < ApplicationController
   def edit
     @user      = current_user
     @donations = @user.donations.unpaid
+    @spotus_donation = @user.current_spotus_donation
   end
 
   def update
     @user = current_user
-    @user.attributes = params[:user]
+    @user.donation_amounts = params[:user][:donation_amounts]
+
     if @user.save
+      @spotus_donation = @user.current_spotus_donation
+      @spotus_donation.amount_in_dollars = params[:user][:spotus_donation_amount]
+      @spotus_donation.save if @spotus_donation.valid?
       redirect_to new_myspot_purchase_path
     else
       @donations = current_user.donations.unpaid
+      @spotus_donation = @user.current_spotus_donation
       render :action => 'edit'
     end
   end
 
 end
+
