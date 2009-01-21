3378486b21dbd860e72c3ff9c354778bb43df45b
erik <dev+jacqui+voxdolo@hashrocket.com>
2009-01-21 16:07:55 -0500

    Removed has_dollar_field;amount_in_cents => amount
    progress on story #360587

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -24,9 +24,6 @@ class Pitch < NewsItem
     validates_presence_of :featured_image_file_name
   end
 
-  has_dollar_field :requested_amount
-  has_dollar_field :current_funding
-
   # Next :accept required because of rails bug:
   # http://skwpspace.com/2008/02/21/validates_acceptance_of-behavior-in-rails-20/
   validates_acceptance_of :contract_agreement, :accept => true, :allow_nil => false
@@ -39,8 +36,8 @@ class Pitch < NewsItem
       find_all_by_user_id(user.id)
     end
 
-    def total_amount_in_cents_for_user(user)
-      for_user(user).map(&:amount_in_cents).sum
+    def total_amount_for_user(user)
+      for_user(user).map(&:amount).sum
     end
   end
   has_many :supporters, :through => :donations, :source => :user, :order => "donations.created_at", :uniq => true
@@ -48,9 +45,9 @@ class Pitch < NewsItem
   before_save :dispatch_fact_checker
   after_save :check_if_funded_state
 
-  named_scope :most_funded, :order => 'news_items.current_funding_in_cents DESC'
+  named_scope :most_funded, :order => 'news_items.current_funding DESC'
   named_scope :featured, :conditions => {:feature => true}
-  named_scope :almost_funded, :order => "(news_items.current_funding_in_cents / news_items.requested_amount_in_cents) desc"
+  named_scope :almost_funded, :order => "(news_items.current_funding / news_items.requested_amount) desc"
   named_scope :sorted, lambda {|direction| { :order => "created_at #{direction}" } }
   named_scope :unpublished, :conditions => 'id NOT IN(SELECT news_item_id FROM news_items WHERE type = "Story" AND status = "published")'
 
@@ -73,7 +70,7 @@ class Pitch < NewsItem
   end
 
   def current_funding_in_percentage
-    (current_funding_in_cents.to_f/requested_amount_in_cents.to_f)
+    (current_funding/requested_amount)
   end
 
   def check_if_funded_state
@@ -92,9 +89,9 @@ class Pitch < NewsItem
     self.update_attribute(:feature, true)
   end
 
-  def funding_needed_in_cents
+  def funding_needed
     return 0 unless active?
-    requested_amount_in_cents - total_amount_donated.to_cents
+    requested_amount - total_amount_donated
   end
 
   def featureable_by?(user)
@@ -103,34 +100,38 @@ class Pitch < NewsItem
 
   def fully_funded?
     return true if accepted? || funded?
-    donations.paid.map(&:amount_in_cents).sum >= requested_amount_in_cents
+    donations.paid.map(&:amount).sum >= requested_amount
+  end
+
+  def requested_amount=(value)
+    self[:requested_amount] = value.to_s.gsub(/[^\d\.]/, "") unless value.nil?
   end
 
   def total_amount_donated
-    donations.paid.map(&:amount_in_cents).sum.to_dollars
+    donations.paid.map(&:amount).sum
   end
 
   def donated_to?
     donations.paid.any?
   end
 
-  def donation_limit_per_user_in_cents
-    requested_amount_in_cents * MAX_PER_USER_DONATION_PERCENTAGE
+  def donation_limit_per_user
+    requested_amount * MAX_PER_USER_DONATION_PERCENTAGE
   end
 
-  def max_donation_amount_in_cents(user)
-    if user.organization? || (funding_needed_in_cents < donation_limit_per_user_in_cents)
-      funding_needed_in_cents
+  def max_donation_amount(user)
+    if user.organization? || (funding_needed < donation_limit_per_user)
+      funding_needed
     else
-      donation_limit_per_user_in_cents
+      donation_limit_per_user
     end
   end
 
-  def user_can_donate_more?(user, attempted_donation_amount_in_cents)
-    return false if attempted_donation_amount_in_cents.nil?
-    return false if attempted_donation_amount_in_cents > funding_needed_in_cents
-    user_donations_in_cents = donations.paid.total_amount_in_cents_for_user(user)
-    (user_donations_in_cents + attempted_donation_amount_in_cents) <= max_donation_amount_in_cents(user)
+  def user_can_donate_more?(user, attempted_donation_amount)
+    return false if attempted_donation_amount.nil?
+    return false if attempted_donation_amount > funding_needed
+    user_donations = donations.paid.total_amount_for_user(user)
+    (user_donations + attempted_donation_amount) <= max_donation_amount(user)
   end
 
   def dispatch_fact_checker
