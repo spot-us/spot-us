17dc940c124abe4e767a538bd9d301b18dbb84a1
GIT_COMMITTER_NAME <dev+jacqui+voxdolo@hashrocket.com>
2009-01-22 16:12:41 -0500

    Remove deprecated options from prod and staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching = true
 config.action_mailer.raise_delivery_errors = false
-config.action_view.cache_template_loading = true
 config.action_view.debug_rjs = false
 config.cache_classes = true
 config.whiny_nils = true
