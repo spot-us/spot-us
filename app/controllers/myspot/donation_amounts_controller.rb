b3310d160dc59d09b6e7794a5f0668693457fafd
GIT_AUTHOR_NAME <dev+jacqui+voxdolo@hashrocket.com>
2009-01-23 16:43:20 -0500

    Refactoring updating and purchasing donations
    resolves story #366538

--- app/controllers/myspot/donation_amounts_controller.rb
+++ app/controllers/myspot/donation_amounts_controller.rb
@@ -1,33 +1,29 @@
 class Myspot::DonationAmountsController < ApplicationController
 
   before_filter :login_required
+  helper_method :unpaid_donations, :spotus_donation
 
-  def edit
-    # current_user (remove @user)
-    # current_user.donations.unpaid.each do |unpaid_donation|
-    #  new_amount = params['donation'][unpaid_donation.id]['amount'] 
-    #  unpaid_donation.update_attribute :amount, new_amount
-    # end
-    #   
-    @user      = current_user
-    @donations = @user.donations.unpaid
-    @spotus_donation = @user.current_spotus_donation
-  end
+  def edit; end
 
   def update
-    @user = current_user
-    @user.donation_amounts = params[:user][:donation_amounts]
-
-    if @user.save
-      @spotus_donation = @user.current_spotus_donation
-      @spotus_donation.amount = params[:user][:spotus_donation_amount]
-      @spotus_donation.save if @spotus_donation.valid?
+    donation_amounts = params[:donation_amounts]
+    @donations = Donation.update(donation_amounts.keys, donation_amounts.values)
+    if @donations.all?{|d| d.valid? }
+      spotus_donation.update_attribute(:amount, params[:spotus_donation_amount])
       redirect_to new_myspot_purchase_path
     else
-      @donations = current_user.donations.unpaid
-      @spotus_donation = @user.current_spotus_donation
       render :action => 'edit'
     end
+  end
+
+  protected
+
+  def unpaid_donations
+    @unpaid_donations ||= current_user.donations.unpaid
+  end
+
+  def spotus_donation
+    @spotus_donation ||= current_user.current_spotus_donation
   end
 
 end
