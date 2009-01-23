cf6cb035f208d1ab4b405f6acf3c5f6bd80c5bcc
GIT_AUTHOR_NAME <dev+jacqui+voxdolo@hashrocket.com>
2009-01-23 11:01:43 -0500

    Bugfix fix: use content_tag and write it's results
    Use content_tag, but follow the new rules. content_tag no longer
    implicitly concatentates it's generated content to the blocks
    binding. It is now necessary to explicitly write the content.

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -1,21 +1,21 @@
-class NewsItemsController < ApplicationController
+class NewsItemsController < ApplicationController  
   include NewsItemsHelper
-
+  
   def index
     get_news_items
   end
-
+  
   def sort_options
     render :text => options_for_sorting(params.fetch(:news_item_type, "news_items"), params.fetch(:sort_by,"desc"))
   end
-
+  
   def search
     get_news_items
     render :action => 'index'
   end
-
+  
   protected
-  def get_news_items
+  def get_news_items   
     unless params[:news_item_type].blank?
       params[:sort_by] = 'desc' unless %w(desc asc most_pledged most_funded almost_funded).include?(params[:sort_by])
       @news_items = params[:news_item_type].camelize.singularize.constantize.send(params[:sort_by]).fundable_news_item
