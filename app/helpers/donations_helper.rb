50d79d8fa4de430ed140f209d51400c29be313db
Stephen Caudill <dev+jacqui+voxdolo@hashrocket.com>
2009-01-30 16:44:15 -0500

    Bugfix: donate more than needed fails silently
    resolves #370465

---
+++ app/helpers/donations_helper.rb
@@ -0,0 +1,9 @@
+module DonationsHelper
+  def donate_text(news_item)
+    if current_user && current_user.has_donated_to?(news_item)
+      'DONATE MORE &raquo;'
+    else
+      'OR DONATE ANOTHER AMOUNT &raquo;'
+    end
+  end
+end
