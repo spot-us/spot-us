d47fd885fbce2f12be6e048c83eeeecfddf6b064
erik <mremsik@gmail.com>
2009-02-17 12:57:43 -0500

    Adding in the bottom field for home page editing also a migration for the data that should be there by default

--- app/models/site_option.rb
+++ app/models/site_option.rb
@@ -1,13 +1,6 @@
 class SiteOption < ActiveRecord::Base
-  include Sanitizy
-
   validates_presence_of :key
   validates_presence_of :value
-
-  cleanse_columns(:value) do |sanitizer|
-    sanitizer.allowed_tags.replace(%w(object param embed a img))
-    sanitizer.allowed_attributes.replace(%w(width height name src value allowFullScreen type href allowScriptAccess style wmode pluginspage classid codebase data quality))
-  end
 
   def self.for(key)
     site_option = find_by_key(key)
