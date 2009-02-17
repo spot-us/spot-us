db95c27a0bb4366b8b15b5cde2f5b2a4ba3847a6
GIT_COMMITTER_NAME <mremsik@gmail.com>
2009-02-17 12:57:43 -0500

    Adding in the bottom field for home page editing also a migration for the data that should be there by default

--- app/helpers/admin/site_options_helper.rb
+++ app/helpers/admin/site_options_helper.rb
@@ -1,11 +1,15 @@
 module Admin::SiteOptionsHelper
-  def video_content
-    SiteOption.for(:video_content)
+  def site_option_content_for(key)
+    SiteOption.for(key)
   end
 
   def change_site_option(key)
     if current_user && current_user.admin?
       site_option = SiteOption.find_or_initialize_by_key(key)
+      if site_option.new_record?
+        site_option.value = 'Default value'
+        site_option.save!
+      end
       link_to "Edit #{key.to_s.humanize}", edit_admin_site_option_path(site_option), :rel => 'facebox'
     end
   end
