99437f291be54095a44a169c57d4f1caaa3f6b54
GIT_COMMITTER_NAME <dev+desi+veez@hashrocket.com>
2009-02-18 09:53:16 -0500

    Add Network

---
+++ spec/models/network_spec.rb
@@ -0,0 +1,12 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Network do
+
+  requires_presence_of Network, :name
+
+  it "should only allow valid URL characters" do
+    network = Network.new(:name => 'invalid /// name')
+    network.should_not be_valid
+    network.should have(1).errors_on(:name)
+  end
+end
