4b052bec214027b7c0e2327353c54719ea1c5d7d
Desi McAdam and Veez (Matt Remsik) <dev+desi+veez@hashrocket.com>
2009-02-18 11:23:16 -0500

    Annotate models

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+# Schema version: 20090218144012
+#
+# Table name: networks
+#
+#  id         :integer(4)      not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Network < ActiveRecord::Base
   validates_presence_of :name
   validates_format_of   :name, :with => /^[a-z0-9|-]+$/i
--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,3 +1,43 @@
+# == Schema Information
+# Schema version: 20090218144012
+#
+# Table name: news_items
+#
+#  id                          :integer(4)      not null, primary key
+#  headline                    :string(255)
+#  location                    :string(255)
+#  state                       :string(255)
+#  short_description           :text
+#  delivery_description        :text
+#  extended_description        :text
+#  skills                      :text
+#  keywords                    :string(255)
+#  deliver_text                :boolean(1)      not null
+#  deliver_audio               :boolean(1)      not null
+#  deliver_video               :boolean(1)      not null
+#  deliver_photo               :boolean(1)      not null
+#  contract_agreement          :boolean(1)      not null
+#  expiration_date             :datetime
+#  created_at                  :datetime
+#  updated_at                  :datetime
+#  featured_image_file_name    :string(255)
+#  featured_image_content_type :string(255)
+#  featured_image_file_size    :integer(4)
+#  featured_image_updated_at   :datetime
+#  type                        :string(255)
+#  video_embed                 :text
+#  featured_image_caption      :string(255)
+#  user_id                     :integer(4)
+#  status                      :string(255)
+#  feature                     :boolean(1)
+#  fact_checker_id             :integer(4)
+#  news_item_id                :integer(4)
+#  deleted_at                  :datetime
+#  widget_embed                :text
+#  requested_amount            :decimal(15, 2)
+#  current_funding             :decimal(15, 2)
+#
+
 class NewsItem < ActiveRecord::Base
   include HasTopics
   include AASMWithFixes
@@ -77,44 +117,4 @@ class NewsItem < ActiveRecord::Base
   end
 end
 
-
-# == Schema Information
-# Schema version: 20090116200734
-#
-# Table name: news_items
-#
-#  id                          :integer(4)      not null, primary key
-#  headline                    :string(255)
-#  location                    :string(255)
-#  state                       :string(255)
-#  short_description           :text
-#  delivery_description        :text
-#  extended_description        :text
-#  skills                      :text
-#  keywords                    :string(255)
-#  deliver_text                :boolean(1)      not null
-#  deliver_audio               :boolean(1)      not null
-#  deliver_video               :boolean(1)      not null
-#  deliver_photo               :boolean(1)      not null
-#  contract_agreement          :boolean(1)      not null
-#  expiration_date             :datetime
-#  created_at                  :datetime
-#  updated_at                  :datetime
-#  featured_image_file_name    :string(255)
-#  featured_image_content_type :string(255)
-#  featured_image_file_size    :integer(4)
-#  featured_image_updated_at   :datetime
-#  type                        :string(255)
-#  video_embed                 :text
-#  featured_image_caption      :string(255)
-#  user_id                     :integer(4)
-#  requested_amount_in_cents   :integer(4)
-#  current_funding_in_cents    :integer(4)      default(0)
-#  status                      :string(255)
-#  feature                     :boolean(1)
-#  fact_checker_id             :integer(4)
-#  news_item_id                :integer(4)
-#  deleted_at                  :datetime
-#  widget_embed                :text
-#
 
