5458fff1e9b8a3bf1a1e952076ba497f0fbf1f2e
Desi McAdam and Veez (Matt Remsik) <dev+desi+veez@hashrocket.com>
2009-02-18 11:15:00 -0500

    Basic crud for networks

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ ActionController::Routing::Routes.draw do |map|
     admin.resources :pitches, :member => { :fact_checker_chooser => :get }
     admin.resources :tips
     admin.resources :site_options
+    admin.resources :networks
   end
 
   map.namespace :myspot do |myspot|
