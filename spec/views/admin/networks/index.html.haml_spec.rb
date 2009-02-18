541c9402040efec6241f152e464eca04a9a386a8
GIT_COMMITTER_NAME <dev+desi+veez@hashrocket.com>
2009-02-18 11:15:00 -0500

    Basic crud for networks

---
+++ spec/views/admin/networks/index.html.haml_spec.rb
@@ -0,0 +1,27 @@
+require File.dirname(__FILE__) + '/../../../spec_helper.rb'
+
+describe "index view" do
+  def do_render
+    render "/admin/networks/index.html.haml"
+  end
+
+  before do
+    @networks = [Factory(:network)]
+    assigns[:networks] = @networks
+  end
+
+  it "should list networks" do
+    do_render
+    response.should have_tag("span.network_name", @networks.first.name)
+  end
+
+  it "should have a link to edit" do
+    do_render
+    response.should have_tag("a[href=?]", edit_admin_network_path(@networks.first), "Edit")
+  end
+
+  it "should have a new link" do
+    do_render
+    response.should have_tag("a[href=?]", new_admin_network_path, 'New network')
+  end
+end
