2e5a7fc84caaa65f06d4c348152c98bf177a5eb6
Veez (Matt Remsik) <dev+desi+veez@hashrocket.com>
2009-02-18 11:15:00 -0500

    Basic crud for networks

---
+++ spec/views/admin/networks/new.html.haml_spec.rb
@@ -0,0 +1,23 @@
+require File.dirname(__FILE__) + '/../../../spec_helper.rb'
+
+describe "new view" do
+
+  def do_render
+    render "/admin/networks/new.html.haml"
+  end
+
+  before do
+    assigns[:network] = Network.new
+  end
+
+  it "should have a name field" do
+    do_render
+    response.should have_tag("input[name=?]", "network[name]")
+  end
+  
+  it "should have a submit field" do
+    do_render
+    response.should have_tag("input[type=?]", "submit")
+  end
+
+end
