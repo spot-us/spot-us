3e2dccf24ff291f2d2f60899479a92399fe39dfd
GIT_COMMITTER_NAME <dev+desi+veez@hashrocket.com>
2009-02-19 14:23:40 -0500

    NetworksController to handle category requests (not admin/)

--- app/controllers/admin/networks_controller.rb
+++ app/controllers/admin/networks_controller.rb
@@ -2,7 +2,6 @@ class Admin::NetworksController < ApplicationController
   resources_controller_for :networks
 
   before_filter :admin_required
-  skip_before_filter :admin_required, :only => :categories
   layout "bare"
 
   response_for :create do |format|
@@ -18,14 +17,6 @@ class Admin::NetworksController < ApplicationController
   response_for :update do |format|
     format.html do
       redirect_to edit_admin_network_path(self.resource)
-    end
-  end
-
-  def categories
-    respond_to do |format|
-      format.json do
-        render :json => Category.find_all_by_network_id(params[:id])
-      end
     end
   end
 
