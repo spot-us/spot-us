da547df6544c8a94293ea4dd36f1da2974586cfd
Veez (Matt Remsik) <dev+desi+veez@hashrocket.com>
2009-02-20 12:09:34 -0500

    Pitches can be browsed by network

--- config/environment.rb
+++ config/environment.rb
@@ -33,7 +33,7 @@ end
 
 # These are the sizes of the domain (i.e. 0 for localhost, 1 for something.com)  
 # for each of your environments  
-SubdomainFu.tld_sizes = { :development => 0,
+SubdomainFu.tld_sizes = { :development => 1,
                           :test => 0,
                           :staging => 2,
                           :production => 1 }
