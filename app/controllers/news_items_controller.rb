496381a4cd6b4c024f0192b98b0a7a11c90b5fda
Desi McAdam and Veez (Matt Remsik) <dev+desi+veez@hashrocket.com>
2009-02-20 09:54:52 -0500

    Refactor search pattern into proper named scopes

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -20,6 +20,9 @@ class NewsItemsController < ApplicationController
   protected
 
   def get_news_items
-    @news_items = NewsItem.by_sort(params[:news_item_type] || 'pitch', params[:sort_by]).paginate(:all, :page => params[:page])
+    model_name = params[:news_item_type]
+    raise "Can only search for valid news item types" and return unless %w(tip pitch news_item).include?(model_name)
+    model = model_name.classify.constantize
+    model.sort_by(params[:sort_by]).paginate(:all, :page => params[:page])
   end
 end
