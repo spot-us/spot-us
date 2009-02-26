e963cfa084e2a8f0180fe0e1363f6901f8b5ec42
GIT_AUTHOR_NAME <dev+veez+voxdolo@hashrocket.com>
2009-02-26 14:01:19 -0500

    Network requires display name

--- spec/models/network_spec.rb
+++ spec/models/network_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Network do
 
   requires_presence_of Network, :name
+  requires_presence_of Network, :display_name
 
   it "should only allow valid URL characters" do
     network = Network.new(:name => 'invalid /// name')
