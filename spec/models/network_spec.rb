30093c32f54e30d64f5d2e5b9b6b9fef6e2a5877
GIT_AUTHOR_NAME <dev+veez+voxdolo@hashrocket.com>
2009-02-26 11:44:27 -0500

    Networks know their pitches (and if they have any)

--- spec/models/network_spec.rb
+++ spec/models/network_spec.rb
@@ -14,4 +14,19 @@ describe Network do
   it "should have many categories" do
     Network.new.categories.should == []
   end
+
+  describe "with_pitches" do
+    it "should return an array of Networks" do
+      Network.with_pitches.all?{|n| n.kind_of?(Network) }.should be_true
+    end
+    it "should not contain duplicates" do
+      Network.with_pitches.uniq.should == Network.with_pitches
+    end
+    it "should only contain networks with pitches" do
+      network1 = Factory(:network)
+      pitch = Factory(:pitch, :network => network1)
+      network2 = Factory(:network)
+      Network.with_pitches.all?{|n| n.pitches.count > 0 }.should be_true
+    end
+  end
 end
