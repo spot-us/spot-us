0402fa564aba33d27d4910e7c5dd72380155f632
Veez (Matt Remsik) <mremsik@gmail.com>
2009-02-27 14:38:56 -0500

    More robust controller for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,25 @@ class PostsController < ApplicationController
 
   response_for :create do |format|
     format.html do
-      redirect_to myspot_posts_path
+      if resource_saved?
+        flash[:success] = 'Successfully created post'
+        redirect_to myspot_posts_path
+      else
+        flash[:error] = 'There was an error saving your post'
+        render :action => 'new'
+      end
+    end
+  end
+
+  response_for :update do |format|
+    format.html do
+      if resource_saved?
+        flash[:success] = 'Successfully updated post'
+        redirect_to myspot_posts_path
+      else
+        flash[:error] = 'There was an error updating your post'
+        render :action => 'edit'
+      end
     end
   end
 
