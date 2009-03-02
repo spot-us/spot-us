70399be8e673b97795d0a0212cf9bc75cd529c4f
GIT_COMMITTER_NAME <mremsik@gmail.com>
2009-03-02 11:30:00 -0500

    Refactored do_render out of each example

--- spec/views/posts/show.html.haml_spec.rb
+++ spec/views/posts/show.html.haml_spec.rb
@@ -8,33 +8,27 @@ describe "showing a blog post" do
     @pitch = Factory(:pitch)
     @post = Factory(:post, :pitch => @pitch)
     assigns[:post] = @post
+    do_render
   end
   it "should link to the pitch" do
-    do_render
     response.should have_tag("a[href=?]", pitch_url(@pitch))
   end
   it "should list the title" do
-    do_render
     response.body.should include(@post.title)
   end
   it "should show the body" do
-    do_render
     response.body.should include(@post.body)
   end
   it "should show the media embed" do
-    do_render
     response.body.should include(@post.media_embed)
   end
   it "should show the user" do
-    do_render
     response.body.should include(@post.pitch.user.full_name)
   end
   it "should include donations button" do
-    do_render
     response.should have_tag("div#inline_donation_form_#{@pitch.id}")
   end
   it "should include the post" do
-    do_render
     response.should have_tag("h4", @post.title)
   end
 end
