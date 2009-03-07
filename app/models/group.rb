a61a5701eb16b988a61199757178b75b77c09e59
GIT_AUTHOR_NAME <mremsik@gmail.com>
2009-03-07 10:14:10 -0500

    Add a medium image size for groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,7 @@ class Group < ActiveRecord::Base
   validates_presence_of :name, :description
 
   has_attached_file :image,
-                    :styles      => { :thumb => '50x50#' },
+                    :styles => { :thumb => '50x50#', :medium => "200x150#" },
                     :default_url => "/images/default_avatar.png",
                     :path        => ":rails_root/public/system/groups/:attachment/:id_partition/:basename_:style.:extension",
                     :url         => "/system/groups/:attachment/:id_partition/:basename_:style.:extension"
