be5600f2418b2fb890986cd94eee366c0e17255c
GIT_AUTHOR_NAME <mremsik@gmail.com>
2009-03-07 09:19:56 -0500

    Admins can edit groups

--- spec/controllers/admin/groups_controller_spec.rb
+++ spec/controllers/admin/groups_controller_spec.rb
@@ -28,5 +28,21 @@ describe Admin::GroupsController do
     post :create, :group => { :name => '', :description => '' }
     response.should render_template('new')
   end
+
+  describe "editing" do
+    before do
+      @group = Factory(:group)
+    end
+    it 'redirects to index on successful group edit' do
+      put :update, :id => @group.id, :group => { :name => 'Villains', :description => "We don't need no stinky spelling" }
+      flash[:success].should == "Success!"
+      response.should redirect_to(admin_groups_path)
+    end
+
+    it "re-renders the new form on failed group edit" do
+      put :update, :id => @group.id, :group => { :name => '', :description => '' }
+      response.should render_template('edit')
+    end
+  end
 end
 
