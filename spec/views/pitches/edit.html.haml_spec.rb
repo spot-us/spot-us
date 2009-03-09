c1979f6bc2a4f222d871f1ec817f5d631c0d7ada
GIT_AUTHOR_NAME <dev+jacqui+veez@hashrocket.com>
2009-03-09 16:16:22 -0400

    Admin can enter license; show license when it is entered

--- spec/views/pitches/edit.html.haml_spec.rb
+++ spec/views/pitches/edit.html.haml_spec.rb
@@ -2,20 +2,39 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/pitches/edit.html.haml" do
   include PitchesHelper
-  
+
   before do
     @pitch = Factory(:pitch)
     assigns[:pitch] = @pitch
+    @current_user = Factory(:admin)
+    template.stub!(:current_user).and_return(@current_user)
   end
 
   it "should render edit form" do
-    render "/pitches/edit.html.haml"
-    response.should have_tag("form[action=#{pitch_path(@pitch)}][method=post]") 
+    do_render
+    response.should have_tag("form[action=#{pitch_path(@pitch)}][method=post]")
   end
 
   it "should render _form partial" do
     template.should_receive(:render).with(:partial => "form")
+    do_render
+  end
+
+  it 'includes a license text area if user.admin?' do
+    @current_user.stub!(:admin?).and_return(true)
+    do_render
+    response.should have_tag('textarea[name=?]', 'pitch[license]')
+  end
+
+  it 'does not include a license text area if !user.admin?' do
+    @current_user.stub!(:admin?).and_return(false)
+    do_render
+    response.should_not have_tag('textarea[name=?]', 'pitch[license]')
+  end
+
+  def do_render
     render "/pitches/edit.html.haml"
   end
+
 end
 
