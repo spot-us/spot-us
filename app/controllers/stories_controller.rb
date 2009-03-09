d4ff659059ec40c878827c1931ea640bd02e36dc
GIT_AUTHOR_NAME <dev+jacqui+veez@hashrocket.com>
2009-03-09 13:01:12 -0400

    Refactor: whitespace

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,36 +1,36 @@
 class StoriesController < ApplicationController
-  before_filter :can_view?, :only => [:show]  
+  before_filter :can_view?, :only => [:show]
   before_filter :can_edit?, :only => :edit
   resources_controller_for :stories
-  
+
   def accept
     story = find_resource
     story.accept!
-    flash[:notice] = "Your story has been submitted to spot.us personnel to be published" 
+    flash[:notice] = "Your story has been submitted to spot.us personnel to be published"
     redirect_back_or_default("/")
   end
-  
+
   def reject
     story = find_resource
     story.reject!
     flash[:notice] = "Your story has been sent back to the reporter for edits"
     redirect_back_or_default("/")
   end
-  
+
   def fact_check
     story = find_resource
     story.verify!
     flash[:notice] = "Your story has been sent to the peer reviewer"
     redirect_back_or_default("/")
   end
-  
+
   def publish
     story = find_resource
     story.publish!
     flash[:notice] = "Your story has been published"
     redirect_back_or_default("/")
   end
-  
+
   protected
     def can_view?
       story = find_resource
@@ -40,21 +40,21 @@ class StoriesController < ApplicationController
           :redirect => pitch_url(story.pitch))
       end
     end
-    
+
     def can_edit?
       story = find_resource
       if current_user.nil?
         access_denied( \
-          :flash => "You cannot edit a story unless you are logged in.", 
+          :flash => "You cannot edit a story unless you are logged in.",
           :redirect => new_session_path)
       end
       unless story.editable_by?(current_user)
         access_denied( \
-          :flash => "You cannot edit this story, contact info@spot.us", 
+          :flash => "You cannot edit this story, contact info@spot.us",
           :redirect => story_url(story))
       end
     end
-    
+
     def find_resources
       @stories = Story.by_network(current_network).published
     end
