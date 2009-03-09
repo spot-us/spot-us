4980225a6c8f1ce0618d466243669aaf8df2dceb
GIT_COMMITTER_NAME <mremsik@gmail.com>
2009-03-09 12:39:38 -0400

    Refactor: network code in lib: network_methods

--- lib/network_methods.rb
+++ lib/network_methods.rb
@@ -1,6 +1,7 @@
-module NetworkValidation
+module NetworkMethods
   def self.included(base)
     base.class_eval do
+      belongs_to :network
       before_validation_on_create :set_default_network
       validates_presence_of :network
       validate :category_in_network
@@ -13,5 +14,12 @@ module NetworkValidation
 
   def category_in_network
     errors.add(:category, "must be part of the selected network") unless network.nil? || network.categories.include?(category) || category.nil?
+  end
+
+  def network_and_category
+    return "No network selected" unless network
+    output =  network.display_name
+    output += "- #{category.name}" if category
+    output
   end
 end
