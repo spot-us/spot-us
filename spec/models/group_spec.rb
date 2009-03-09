cebb15a9299d4a08e9acc943bd0049f96c2e09b8
erik <mremsik@gmail.com>
2009-03-09 10:31:52 -0400

    Group knows about donors, pitches, and amounts

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -24,4 +24,71 @@ describe Group do
     end
   end
 
+  describe "donors" do
+    before do
+      @group = Factory(:group)
+      @user1 = Factory(:citizen)
+      @user2 = Factory(:citizen)
+      Factory(:donation, :user => @user1, :group => @group, :status => 'paid')
+      Factory(:donation, :user => @user1, :group => @group, :status => 'paid')
+    end
+    it "should return those users who have made donations for this group" do
+      @group.donors.size.should == 1
+    end
+    it "should not include users who have not given for this group" do
+      @group.donors.should_not include(@user2)
+    end
+    it "should return a unique set of donors" do
+      @group.donors.should == [@user1]
+    end
+  end
+
+  describe "total_donations" do
+    before do
+      @group = Factory(:group)
+      Factory(:donation, :amount => 10, :group => @group, :status => 'paid')
+      Factory(:donation, :amount => 10, :group => @group, :status => 'paid')
+    end
+    it 'returns the sum of donations for the group' do
+      @group.total_donations.should == BigDecimal("20")
+    end
+    it 'returns 0 if no donations made for the group' do
+      Factory(:group).total_donations.should == BigDecimal("0")
+    end
+  end
+
+  describe "amount_donated_by(user)" do
+    before do
+      @group = Factory(:group)
+      @user1 = Factory(:citizen)
+      @user2 = Factory(:citizen)
+      @donation = Factory(:donation, :group => @group, :user => @user1, :status => 'paid')
+    end
+    it "returns the amount a user donated" do
+      @group.amount_donated_by(@user1).should == @donation.amount
+    end
+    it "returns 0 if the user hasn't donated on behalf of the group" do
+      @group.amount_donated_by(@user2).should == BigDecimal("0")
+    end
+  end
+
+  describe "pitches" do
+    before do
+      @group = Factory(:group)
+      @pitch1 = Factory(:pitch)
+      @pitch2 = Factory(:pitch)
+      Factory(:donation, :group => @group, :pitch => @pitch1)
+      Factory(:donation, :group => @group, :pitch => @pitch1)
+    end
+    it "should return pitches that are associated with this groups donations" do
+      @group.pitches.should include(@pitch1)
+    end
+    it "should not return other pitches" do
+      @group.pitches.should_not include(@pitch2)
+    end
+    it "should return a unique set of pitches" do
+      @group.pitches.should == [@pitch1]
+    end
+  end
+
 end
