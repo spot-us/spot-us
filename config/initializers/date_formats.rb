296e42ae9436ad08e7aae8105597e97914561c67
Veez (Matt Remsik) <mremsik@gmail.com>
2009-03-10 18:19:27 -0400

    Reporting team: Approve member to blog / restyle admin/pitches

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,6 +1,7 @@
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
-	:date => '%B %e, %Y',
-	:date_time  => "%m/%d/%Y %I:%M%p",
-  :long_date => '%A, %B %e, %Y'
+  :date => '%B %e, %Y',
+  :date_time  => "%m/%d/%Y %I:%M%p",
+  :long_date => '%A, %B %e, %Y',
+  :super_short => '%m/%d/%y'
 )
 
