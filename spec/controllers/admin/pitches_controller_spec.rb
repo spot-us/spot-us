5c0185cd28304617893c8569bededb5a0716ef89
GIT_COMMITTER_NAME <dev+jacqui+veez@hashrocket.com>
2009-03-10 12:55:31 -0400

    Pitch approval: admin pitches index

--- spec/controllers/admin/pitches_controller_spec.rb
+++ spec/controllers/admin/pitches_controller_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Admin::PitchesController do
-  route_matches('/admin/pitches', :get, :controller => 'admin/pitches',
-                                     :action     => 'index')
+  route_matches('/admin/pitches', :get, :controller => 'admin/pitches', :action => 'index')
+  route_matches('/admin/pitches/1/approve', :put, :controller => 'admin/pitches', :action => 'approve', :id => '1')
+  route_matches('/admin/pitches/1/unapprove', :put, :controller => 'admin/pitches', :action => 'unapprove', :id => '1')
 end
