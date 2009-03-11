c9a2f80e9272af9922ba5436f3ce02e094ec7cab
GIT_AUTHOR_NAME <dev+jacqui+veez@hashrocket.com>
2009-03-11 10:27:42 -0400

    Admin can unapprove bloggers for a pitch

--- spec/views/admin/pitches/index.html.haml_spec.rb
+++ spec/views/admin/pitches/index.html.haml_spec.rb
@@ -1,12 +1,36 @@
 require File.dirname(__FILE__) + "/../../../spec_helper"
 
 describe '/admin/pitches/index' do
-  it "should render" do
+  before do
     @user = Factory(:admin)
     template.stub!(:current_user).and_return(@user)
-    pitches = [Factory(:pitch, :expiration_date => Time.now + 1.day), 
-               Factory(:pitch, :expiration_date => Time.now + 1.day)]
-    assigns[:pitches] = pitches
+    @pitches = [Factory(:pitch, :expiration_date => Time.now + 1.day), 
+                Factory(:pitch, :expiration_date => Time.now + 1.day)]
+    assigns[:pitches] = @pitches
+  end
+
+  it "should render" do
+    do_render
+  end
+
+  it "should include a list of blog applicants for each pitch" do
+    user = Factory(:citizen)
+    contributors = stub('association', :unapproved => [Factory(:contributor_application, :user => user)])
+    @pitches.first.stub!(:contributor_applications).and_return(contributors)
+    do_render
+    response.should have_tag("select[name='user_id']") do
+      with_tag("option", user.full_name)
+    end
+  end
+
+  it "should link to unapproved each already-approved blogger" do
+    user = Factory(:citizen)
+    @pitches.first.stub!(:contributors).and_return([user])
+    do_render
+    response.should have_tag("a[href=?]", unapprove_blogger_admin_pitch_path(@pitches.first, :user_id => user.id), '(unapprove)')
+  end
+
+  def do_render
     render '/admin/pitches/index'
   end
 end
