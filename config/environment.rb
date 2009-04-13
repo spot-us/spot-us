d2117ee868e7a8d7ea6d623ed79fac30aa11fbca
GIT_AUTHOR_NAME <dev+jacqui+veez@hashrocket.com>
2009-04-13 17:47:07 -0400

    Updated rspec to 1.2.2

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
 require 'yaml' 
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
   config.gem "rubyist-aasm", :lib => "aasm", :version => '>=2.0.5', :source => 'http://gems.github.com'
   config.gem 'mislav-will_paginate', :lib => 'will_paginate', :version => '>=2.3.7', :source => 'http://gems.github.com/'
-  config.gem "rspec-rails", :lib => false, :version => "= 1.1.11"
+  config.gem "rspec-rails", :lib => false, :version => "= 1.2.2"
   config.gem "cucumber", :lib => false, :version => "= 0.1.12"
   config.gem "webrat", :lib => false, :version => "= 0.3.2"
 
