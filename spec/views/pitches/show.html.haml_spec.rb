56b649c99615a890e65151a2eaf62772e5427365
erik <dev+jacqui+veez@hashrocket.com>
2009-04-14 13:03:20 -0400

    Join reporting team should link to facebox'd login if no current_user exists

--- spec/views/pitches/show.html.haml_spec.rb
+++ spec/views/pitches/show.html.haml_spec.rb
@@ -311,6 +311,11 @@ describe "/pitches/show.html.haml" do
         do_render
         response.should have_tag("a[href=?]", apply_to_contribute_pitch_path(@pitch))
       end
+      it "Join Reporting Team button should link to login in a facebox if necessary" do
+        template.stub!(:current_user).and_return(nil)
+        do_render
+        response.should have_tag("a[href=?][rel='facebox']", new_session_path)
+      end
       it "should not show when the current user is the reporter for the pitch" do
         template.stub!(:current_user).and_return(@pitch.user)
         do_render
