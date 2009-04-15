ad9a4437f6719aa4bed173899117ba282bcf1461
erik <dev+jacqui+veez@hashrocket.com>
2009-04-15 16:53:58 -0400

    Add paypal actions to purchases controller

--- spec/controllers/myspot/purchases_controller_spec.rb
+++ spec/controllers/myspot/purchases_controller_spec.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+include ActiveMerchant::Billing::Integrations
 
 describe Myspot::PurchasesController do
 
@@ -211,83 +212,125 @@ describe Myspot::PurchasesController do
 
   describe "PayPal response" do
     before do
-      @params =  {"protection_eligibility"=>"Eligible", 
-        "tax"=>"0.00",
-        "payment_status"=>"Completed",
-        "address_name"=>"Test User",
-        "business"=>"dev+pa_1239041752_biz@hashrocket.com",
-        "address_country"=>"United States",
-        "address_city"=>"San Jose",
-        "payer_email"=>"dev+pa_1239041717_per@hashrocket.com",
-        "receiver_id"=>"HQ79EJ4KFPWAU",
-        "residence_country"=>"US",
-        "payment_gross"=>"440.00",
-        "mc_shipping"=>"0.00",
-        "receiver_email"=>"dev+pa_1239041752_biz@hashrocket.com",
-        "mc_gross_1"=>"200.00",
-        "address_street"=>"1 Main St",
+      @paypal_params =  {"protection_eligibility"=>"Eligible", "tax"=>"0.00",
+        "payment_status"=>"Completed", "address_name"=>"Test User", 
+        "business"=>"dev+pa_1239041752_biz@hashrocket.com", "address_country"=>"United States", 
+        "address_city"=>"San Jose", "payer_email"=>"dev+pa_1239041717_per@hashrocket.com", 
+        "receiver_id"=>"HQ79EJ4KFPWAU", "residence_country"=>"US", "payment_gross"=>"240.00", 
+        "mc_shipping"=>"0.00", "receiver_email"=>"dev+pa_1239041752_biz@hashrocket.com", 
+        "mc_gross_1"=>"200.00", "address_street"=>"1 Main St",
         "mc_handling1"=>"0.00",
-        "verify_sign"=>"AVv2fa0VdjSSQ6PhgLCpIgZ3PpygAdpa6bB9AXSMUbqY5xLKMkkniUrh",
-        "mc_gross_2"=>"200.00",
-        "address_zip"=>"95131",
-        "mc_handling2"=>"0.00",
-        "item_name1"=>"PITCH: testing testing",
-        "txn_type"=>"cart",
-        "mc_currency"=>"USD",
-        "mc_gross_3"=>"40.00",
-        "transaction_subject"=>"Shopping Cart",
-        "charset"=>"windows-1252",
-        "address_country_code"=>"US",
-        "mc_handling3"=>"0.00",
-        "txn_id"=>"0PB07920SY555151E",
-        "item_name2"=>"PITCH: testing testing",
-        "item_number1"=>"",
-        "notify_version"=>"2.8",
-        "payer_status"=>"verified",
-        "tax1"=>"0.00",
-        "address_state"=>"CA",
-        "payment_fee"=>"13.06",
-        "item_name3"=>"Support Spot.Us",
-        "quantity1"=>"1",
-        "address_status"=>"confirmed",
-        "item_number2"=>"",
-        "payment_date"=>"13:16:29 Apr 14, 2009 PDT",
-        "mc_handling"=>"0.00",
-        "mc_fee"=>"13.06",
-        "tax2"=>"0.00",
-        "quantity2"=>"1",
-        "item_number3"=>"",
-        "first_name"=>"Test",
-        "num_cart_items"=>"3",
-        "mc_shipping1"=>"0.00",
-        "tax3"=>"0.00",
-        "payment_type"=>"instant",
-        "quantity3"=>"1",
-        "test_ipn"=>"1",
-        "mc_gross"=>"440.00",
-        "payer_id"=>"XABGXAPTSL7QS",
-        "mc_shipping2"=>"0.00",
-        "last_name"=>"User",
-        "custom"=>"",
-        "mc_shipping3"=>"0.00"}
+        "verify_sign"=>"A0WBa-tiI74WL8XPUwsW.95bvjWUAaAFF-mwVweS-RtjNFGpKWiQmUQn",
+        "mc_gross_2"=>"40.00", "address_zip"=>"95131", "mc_handling2"=>"0.00",
+        "item_name1"=>"PITCH: testing testing", "txn_type"=>"cart",
+        "mc_currency"=>"USD", "transaction_subject"=>"Shopping Cart",
+        "charset"=>"windows-1252", "address_country_code"=>"US",
+        "txn_id"=>"28C98632UU123291R", "item_name2"=>"Support Spot.Us",
+        "item_number1"=>"128", "notify_version"=>"2.8",
+        "payer_status"=>"verified", "tax1"=>"0.00", "address_state"=>"CA",
+        "payment_fee"=>"7.26", "quantity1"=>"1", "address_status"=>"confirmed",
+        "item_number2"=>"14", "payment_date"=>"06:26:34 Apr 15, 2009 PDT",
+        "mc_handling"=>"0.00", "mc_fee"=>"7.26", "tax2"=>"0.00",
+        "quantity2"=>"1", "first_name"=>"Test", "num_cart_items"=>"2",
+        "mc_shipping1"=>"0.00", "payment_type"=>"instant", "test_ipn"=>"1",
+        "mc_gross"=>"240.00", "payer_id"=>"XABGXAPTSL7QS",
+        "mc_shipping2"=>"0.00", "last_name"=>"User", "custom"=>""}
     end
 
     describe "when the payment has been completed" do
-      it "posts to the paypal url with an additional cmd parameter" do
-        HTTParty.expects(:post).with(PAYPAL_POST_URL, @params.merge({"cmd" => "_notify_validate"}))
-        post :paypal_response, @params
+      def do_post
+        post :paypal_ipn, @paypal_params
+      end
+      before do
+        @user = mock_model(User)
+        @donation = mock_model(Donation, :amount => 200.00, :unpaid? => true, :user => @user)
+        @donations = [@donation]
+        @spotus_donation = mock_model(SpotusDonation, :amount => 40.00, :unpaid? => true, :user => @user)
+        SpotusDonation.stub!(:find_from_paypal).and_return(@spotus_donation)
+        Donation.stub!(:find_all_from_paypal).and_return(@donations)
+        @purchase = mock_model(Purchase, :total_amount => 240.00)
+        @purchase.stub!(:donations=)
+        @purchase.stub!(:spotus_donation=)
+        @purchase.stub!(:user=)
+        Purchase.stub!(:new).and_return(@purchase)
+        @notify = stub('notify', :params => @paypal_params, :acknowledge => false, :amount => "240")
+        Paypal::Notification.stub!(:new).and_return(@notify)
+      end
+      it "finds spotus donation" do
+        SpotusDonation.should_receive(:find_from_paypal).with(@paypal_params).and_return(@spotus_donation)
+        do_post
+      end
+      it "finds the donation" do
+        Donation.should_receive(:find_all_from_paypal).and_return(@donations)
+        do_post
+      end
+      it "logs an error if the donations don't all have the same user" do
+        @donations << mock_model(Donation, :user => mock_model(User))
+        controller.logger.should_receive(:error).with('Invalid users for PayPal transaction 28C98632UU123291R')
+        do_post
+      end
+      it "creates a new purchase" do
+        Purchase.should_receive(:new).and_return(@purchase)
+        @purchase.should_receive(:donations=).with(@donations)
+        @purchase.should_receive(:spotus_donation=).with(@spotus_donation)
+        @purchase.should_receive(:user=).with(@user)
+        do_post
+      end
+      it "acknowledges the notification" do
+        @notify.should_receive(:acknowledge)
+        do_post
+      end
+      it "doesn't create a purchase if any donations are unpaid" do
+        Purchase.stub!(:valid_donations_for_user?).and_return(false)
+        Purchase.should_receive(:new).exactly(0)
+        do_post
+      end
+      it "logs an error if the acknowledgement fails" do
+        @notify.should_receive(:acknowledge).and_return(false)
+        controller.logger.should_receive(:error).with("Failed to verify Paypal's notification, please investigate: 28C98632UU123291R")
+        do_post
+      end
+      it "saves the purchase if paypal verifies the payment and the total is correct" do
+        @notify.should_receive(:acknowledge).and_return(true)
+        @notify.should_receive(:complete?).and_return(true)
+        @purchase.should_receive(:save)
+        do_post
+      end
+      it "doesn't save a purchase otherwise" do
+        @notify.should_receive(:acknowledge).and_return(true)
+        @notify.should_receive(:complete?).and_return(false)
+        @purchase.should_receive(:save).never
+        controller.logger.stub!(:error)
+        do_post
+      end
+      it "logs an error if paypal doesn't verify the payment or the total is incorrect" do
+        controller.logger.should_receive(:error).with("PayPal acknowledgement was unpaid or the amounts didn't match for the following transaction: 28C98632UU123291R").once
+        @notify.stub!(:acknowledge).and_return(true)
+        @notify.stub!(:complete?).and_return(false)
+        do_post
       end
     end
 
-    describe "when a payment was been successful" do
-      it "creates a purchase"
-      it "marks donations paid"
-      it "marks the spot-us donation paid"
-    end
-
-    describe "when a payment was unsuccessful" do
-      it "does not mark donations paid"
-      it "does not mark the spot-us donation paid"
+    describe "#paypal_return" do
+      before do
+        @user = mock_model(User)
+        @donations = stub('named scope', :unpaid => [])
+        @user.stub!(:donations).and_return(@donations)
+        controller.stubs(:current_user).returns(@user)
+      end
+      it "updates the balance cookie" do
+        controller.should_receive(:update_balance_cookie)
+        get :paypal_return
+      end
+      it "shows a success message if the user's donations are all paid" do
+        get :paypal_return
+        flash[:success].should == "Thanks! Your payment has been received."
+      end
+      it "shows a pending message if the user has unpaid donations still" do
+        @user.donations.unpaid.stub!(:any?).and_return(true)
+        get :paypal_return
+        flash[:success].should == "Thanks! Your donations will be marked paid when PayPal clears them."
+      end
     end
   end
 
