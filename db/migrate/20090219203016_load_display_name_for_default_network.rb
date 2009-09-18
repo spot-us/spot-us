8535f39244b62060ec6a3447ef8c10dbc2f39afa
erik <dpnewman@gmail.com>
2009-09-18 12:57:16 -0700

    Organizations can now pitch

--- db/migrate/20090219203016_load_display_name_for_default_network.rb
+++ db/migrate/20090219203016_load_display_name_for_default_network.rb
@@ -1,5 +1,6 @@
 class LoadDisplayNameForDefaultNetwork < ActiveRecord::Migration
   def self.up
+    #Network.reset_column_information
     Network.create!(:name => 'sfbay', :display_name => 'Bay Area')
   end
 
