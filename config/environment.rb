750d47472113977b021a6362e5bd1e9ec89dce84
Dan Newman <dpnewman@gmail.com>
2009-09-18 13:54:08 -0700

    b4 removing environment.rb from repo

--- config/environment.rb
+++ config/environment.rb
@@ -17,7 +17,7 @@ DEFAULT_HOST = APP_CONFIG[:default_host] || "spotus.local"
 Rails::Initializer.run do |config|
   config.gem "haml", :version => '>=2.0.6'
   config.gem "fastercsv"
-  config.gem "mysql", :version => '2.7'
+  #config.gem "mysql", :version => '2.7'
   config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
   config.gem "rubyist-aasm", :lib => "aasm", :version => '>=2.0.5', :source => 'http://gems.github.com'
   config.gem 'mislav-will_paginate', :lib => 'will_paginate', :version => '>=2.3.7', :source => 'http://gems.github.com/'
