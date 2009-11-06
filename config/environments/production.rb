f13ea341efd5151124b236bba7dd7ebcc4b99ff2
neo <nothing@nothing.com>
2009-11-06 13:20:46 -0800

    correcting some settings files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,3 @@ config.after_initialize do
 end
 PAYPAL_POST_URL = "https://www.paypal.com/cgi-bin/webscr"
 PAYPAL_EMAIL = "info@spot.us"
-
-email_settings = YAML::load(File.open("#{RAILS_ROOT}/config/email.yml"))
-ActionMailer::Base.smtp_settings = email_settings[RAILS_ENV] unless email_settings[RAILS_ENV].nil?
