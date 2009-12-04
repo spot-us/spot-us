177f116d07a3053950ff583cbd975b241eac4396
Dan Newman <dpnewman@gmail.com>
2009-12-04 10:58:46 -0800

    Assignments version 1

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -8,7 +8,7 @@ class AssignmentsController < ApplicationController
     format.html do
       if resource_saved?
         flash[:success] = 'Successfully created assignment'
-        redirect_to assignment_path(@assignment)
+        redirect_to pitch_assignment_path(@assignment.pitch, @assignment)
       else
         flash[:error] = 'There was an error saving your assignment'
         render :action => 'new'
@@ -20,19 +20,81 @@ class AssignmentsController < ApplicationController
     format.html do
       if resource_saved?
         flash[:success] = 'Successfully updated assignment'
-        redirect_to assignment_path(@assignment)
+        redirect_to pitch_assignment_path(@assignment.pitch, @assignment)
       else
         flash[:error] = 'There was an error updating your assignment'
         render :action => 'edit'
       end
     end
   end
-
-  # response_for :destroy do |format|
-  #   format.html do
-  #     redirect_to myspot_posts_path
-  #   end
-  # end
+  
+  def process_application
+    assignment = Assignment.find(params[:id])
+    redirect_to :back if !assignment || assignment.is_closed?
+    respond_to do |format|
+      format.html do
+        if assignment.process_application(current_user)
+          flash[:success] = 'You have Successfully applied as a contributor for this assignment. The creator if this pitch will be notified.'
+          redirect_to pitch_assignment_path(assignment.pitch, assignment)
+        else
+          flash[:error] = 'There was an error applying for this assignment'
+          redirect_to pitch_assignments_path(assignment.pitch)
+        end
+      end
+    end
+  end
+  
+  def accept_application
+    assignment = Assignment.find(params[:assignment_id])
+    redirect_to :back if !assignment || assignment.is_closed?
+    application = AssignmentContributor.find(params[:id])
+    redirect_to pitch_assignments_path(assignment.pitch) if assignment.user != current_user || !application 
+    if application.accept
+      flash[:success] = 'Application status is now set to "accepted"'
+      redirect_to pitch_assignment_path(assignment.pitch, assignment)
+    else
+      flash[:error] = 'There was an error changing application status'
+      redirect_to pitch_assignments_path(assignment.pitch)
+    end
+  end
+  
+  def reject_application
+    assignment = Assignment.find(params[:assignment_id])
+    redirect_to :back if !assignment || assignment.is_closed?
+    application = AssignmentContributor.find(params[:id])
+    redirect_to pitch_assignments_path(assignment.pitch) if assignment.user != current_user || !application
+    if application.reject
+      flash[:success] = 'Application status is now set to "rejected"'
+      redirect_to pitch_assignment_path(assignment.pitch, assignment)
+    else
+      flash[:error] = 'There was an error changing application status'
+      redirect_to pitch_assignments_path(assignment.pitch)
+    end
+  end
+    
+  def open_assignment
+    @assignment = Assignment.find(params[:id])
+    redirect_to :back if !@assignment
+    redirect_to pitch_assignments_path(@assignment.pitch) if @assignment.user != current_user
+    if @assignment.open
+      flash[:success] = 'Assignment status is now set to "open".'
+    else
+      flash[:success] = 'Assignment status was unable to be changed.'
+    end
+    redirect_to pitch_assignment_path(@assignment.pitch, @assignment)
+  end
+  
+  def close_assignment
+    @assignment = Assignment.find(params[:id])
+    redirect_to :back if !@assignment
+    redirect_to pitch_assignments_path(@assignment.pitch) if @assignment.user != current_user
+    if @assignment.close
+      flash[:success] = 'Assignment status is now set to "closed".'
+    else
+      flash[:success] = 'Assignment status was unable to be changed.'
+    end
+    redirect_to pitch_assignment_path(@assignment.pitch, @assignment)
+  end
 
   private
 
@@ -42,7 +104,7 @@ class AssignmentsController < ApplicationController
     end
   end
 
-  def authorized?
-    current_user && enclosing_resource.assignable_by?(current_user)
-  end
+  # def authorized?
+  #   current_user && enclosing_resource.assignable_by?(current_user)
+  # end
 end
