7a0ea605cd977e47642f3b0027815799078507d2
GIT_COMMITTER_NAME <nothing@nothing.com>
2010-01-21 18:01:20 -0800

    adding rss for /blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,6 +5,13 @@ class BlogsController < ApplicationController
 
   def index
     @posts = Post.by_network(@current_network).paginate(:page => params[:page], :order => "posts.id desc", :per_page=>10)
+    respond_to do |format|
+      format.rss do
+        render :layout => false
+      end
+      format.html do
+      end
+    end
   end
 
   private
