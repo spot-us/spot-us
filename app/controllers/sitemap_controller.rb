8dc44c478199f7b66912138369a67c495661b12b
GIT_AUTHOR_NAME <nothing@nothing.com>
2010-02-04 23:10:43 -0800

    first draft of Google News sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,27 +1,36 @@
 class SitemapController < ApplicationController
   
   def index
-    sitemap_types = {'posts'=>true, 'stories'=>true, 'pitches'=>true}
-    unless sitemap_types[params[:type]]
-      #redirect to landing page or better present 404
-      redirect_to '/'
-    end 
-    
-    @type = params[:type]
-    case @type
-      when 'posts'
-        @posts = Post.find(:all)
-      when 'stories'
-        @stories = Story.published.find(:all)
-      when 'pitches'
-        @pitches = Pitch.browsable.find(:all)
-    end
-    
-    render :layout=>false
-    
+    @url_set = {:xmlns=>"http://www.sitemaps.org/schemas/sitemap/0.9"}
+    @news ||= params[:news]
+    @url_set["xmlns:n".to_sym] = "http://www.google.com/schemas/sitemap-news/0.9" if @news
+    @prefix_template = @news ? "news_" : ""
+    initialize_sitemap
   end
   
   def sitemap_index
   end
   
+  protected
+
+   def initialize_sitemap
+     sitemap_types = {'posts'=>true, 'stories'=>true, 'pitches'=>true}
+     unless sitemap_types[params[:type]]
+       #redirect to landing page or better present 404
+       redirect_to '/'
+     end 
+
+     @type = params[:type]
+     case @type
+       when 'posts'
+         @posts = Post.find(:all)
+       when 'stories'
+         @stories = Story.published.find(:all)
+       when 'pitches'
+         @pitches = Pitch.browsable.find(:all)
+     end
+
+     render :layout=>false
+   end
+   
 end
