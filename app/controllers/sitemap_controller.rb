e6b6d0371be168dbb41ce70298e4b0cf0afc5106
erik <nothing@nothing.com>
2010-02-04 20:26:42 -0800

    adding stories and pitches as sitemaps

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,16 +1,20 @@
 class SitemapController < ApplicationController
   
   def index
-    sitemap_types = {'posts'=>true}
+    sitemap_types = {'posts'=>true, 'stories'=>true, 'pitches'=>true}
     unless sitemap_types[params[:type]]
       #redirect to landing page or better present 404
+      redirect_to '/'
     end 
     
     @type = params[:type]
-    
     case @type
       when 'posts'
         @posts = Post.find(:all)
+      when 'stories'
+        @stories = Story.published.find(:all)
+      when 'pitches'
+        @pitches = Pitch.browsable.find(:all)
     end
     
     render :layout=>false
