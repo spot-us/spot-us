d72c40ea408ea08903b9f406763a1f460718b037
GIT_AUTHOR_NAME <dpnewman@gmail.com>
2010-02-08 13:32:31 -0800

    admin optimizations and fixes

--- app/controllers/admin/tips_controller.rb
+++ app/controllers/admin/tips_controller.rb
@@ -4,4 +4,7 @@ class Admin::TipsController < ApplicationController
   
   resources_controller_for :tips, :only => [:index, :destroy]
   
+  def index
+    @tips = Tip.paginate(:page => params[:page], :per_page => 40, :order => "created_at desc", :include => [:network, :pledges, :supporters, :category])
+  end
 end
