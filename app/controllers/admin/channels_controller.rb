448f27395feee9b78e814ee506395ffc5e9a00c6
GIT_COMMITTER_NAME <nothing@nothing.com>
2010-02-09 12:03:52 -0800

    adding a better DB solution for getting channels from networks.

--- app/controllers/admin/channels_controller.rb
+++ app/controllers/admin/channels_controller.rb
@@ -34,6 +34,14 @@ class Admin::ChannelsController < ApplicationController
     @pitch = Pitch.find(params[:pitch_id])
     @channel = Channel.find(params[:id])
     @channel.pitches << @pitch
+    # add relation for channel/network if available
+    cn = ChannelsNetwork.find_by_network_id_and_channel_id(@pitch.network_id, @channel.id)
+    unless cn
+      cn = ChannelsNetwork.new
+      cn.network_id = @pitch.network_id
+      cn.channel_id = @channel.id
+      cn.save
+    end
     redirect_to admin_channel_path(@channel)
   end
   
@@ -41,6 +49,9 @@ class Admin::ChannelsController < ApplicationController
     @pitch = Pitch.find(params[:pitch_id])
     @channel = Channel.find(params[:id])
     @channel.pitches.delete(@pitch)
+    # clean up the channel/network associations
+    network_ids = @channel.pitches.find(:all, :select=>"network_id, channel_pitches.created_at as created_at").map(&:network_id).join(",")
+    ChannelsNetwork.delete_all(["network_id not in (?) and channel_id=?", network_ids, @channel.id])
     redirect_to admin_channel_path(@channel)
   end
 
