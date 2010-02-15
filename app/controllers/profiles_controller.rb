0d20e5b9ac65cc0d4c8e52e2c8dfc8bbdf28d5f8
Dan Newman <dpnewman@gmail.com>
2010-02-15 14:39:16 -0800

    user public profile expanded

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,19 @@
 class ProfilesController < ApplicationController
-  resources_controller_for :profiles, :class => User, :only => [:show]
-  before_filter :redirect_appropriately, :except => [:show]
-
+  #resources_controller_for :profiles, :class => User #, :only => [:show]
+  #before_filter :redirect_appropriately, :except => [:show]
+  before_filter :get_profile
   private
+  
+
 
   def redirect_appropriately
     redirect_to(logged_in? ? myspot_profile_path : root_path)
+  end
+  
+  def get_profile
+    @profile = User.find(params[:id])
+    @tab = params[:action]
+    render :template => "/profiles/tab"
   end
 
 end
