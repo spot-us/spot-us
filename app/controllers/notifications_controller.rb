daaaf0527bec427a4da1fb07b2574e3a3d2fc48c
erik <nothing@nothing.com>
2010-02-18 10:57:11 -0800

    adding notifications framework and also updating the settings example to fit it all

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,23 @@
+class NotificationsController < ApplicationController
+  
+  def index
+    #nothing here if you have the wrong hash to pass in here :-)
+    unless params[:code]==APP_SETTINGS[:cron_job_code]
+      response.headers["Status"] = "404 Page Not Found"
+      render :text=>"Ooops, something doesn't exist there."
+      return
+    end
+    notify_pitch_owners
+    render :text=>"ok!" 
+  end
+  
+  private
+  
+  def notify_pitch_owners
+    posts = Post.find(:all, :conditions=> "created_at>='#{1.year.ago}'" ).map(&:pitch_id).uniq
+    Pitch.unfunded.find(:all, :conditions => "id not in (#{posts.join(',')})").each do |pitch|
+      Mailer.deliver_create_blog_post_notification(pitch)
+    end
+  end
+  
+end
---
+++ test/unit/helpers/admin/sections_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Admin::SectionsHelperTest < ActionView::TestCase
+end
