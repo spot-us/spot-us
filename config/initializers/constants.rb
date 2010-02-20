d28d67b5cb388be5dda295128c526506b988e6ab
erik <nothing@nothing.com>
2010-02-19 20:37:47 -0800

    refactoring the filters so they are ordered and the routes self adjust to additions

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -18,3 +18,18 @@ MODEL_NAMES = {
   'funded' => 'Pitch',
   'published' => 'Story'
 }
+
+FILTERS_STORIES = ActiveSupport::OrderedHash.new
+FILTERS_STORIES["unfunded"]="Unfunded"
+FILTERS_STORIES["almost-funded"]="Almost Funded"
+FILTERS_STORIES["funded"]="Funded"
+FILTERS_STORIES["published"]="Published"
+FILTERS_STORIES["suggested"]="Suggested"
+FILTERS_STORIES_STRING = FILTERS_STORIES.collect{ |key, value| key}.join('|')
+
+FILTERS_CONTRIBUTORS = ActiveSupport::OrderedHash.new
+FILTERS_CONTRIBUTORS["donated"]="Recently Donated"
+FILTERS_CONTRIBUTORS["donated-most"]="Donated Most"
+FILTERS_CONTRIBUTORS["organizations"]="Organizations"
+FILTERS_CONTRIBUTORS["reporters"]="Reporters"
+FILTERS_CONTRIBUTORS_STRING = FILTERS_CONTRIBUTORS.collect{ |key, value| key}.join('|')
