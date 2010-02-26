d27ca1265333820282873b227cc94f1f7f117c78
erik <github@sundelof.com>
2010-02-26 15:05:03 -0800

    adding the proper footer and also no more bcc of emails

--- app/models/story.rb
+++ app/models/story.rb
@@ -137,7 +137,13 @@ class Story < NewsItem
   end
   
   def notify_editor
-    Mailer.deliver_story_to_editor_notification(self,fact_checker_recipients)
+    if self.pitch && self.pitch.fact_checker_id
+        fact_checker = User.find_by_id(self.pitch.fact_checker_id)
+        if fact_checker && fact_checker.email
+            Mailer.deliver_story_to_editor_notification(self,fact_checker.full_name,fact_checker.email)
+        end
+    end
+    Mailer.deliver_story_to_editor_notification(self,"David","david@spot.us")
   end
   
   def notify_reporter
@@ -146,7 +152,28 @@ class Story < NewsItem
   
   def notify_donors
     self.pitch.touch_pitch!
-    Mailer.deliver_story_published_notification(self,fact_checker_recipients)
+    #email supporters
+    emails = self.pitch.supporters.map{ |email| "'#{email}'"}
+    self.pitch.supporters.each do |supporter|
+      Mailer.deliver_story_published_notification(self, supporter.full_name, supporter.email)
+    end
+    #email admins
+    emails = emails.concat(Admin.all.map{ |email| "'#{email}'"}).uniq
+    Admin.find(:all,:conditions=>"email not in (#{emails.join(',')})").each do |admin|
+      Mailer.deliver_story_published_notification(self, admin.full_name, admin.email)
+    end
+    #email subscribers
+    emails = emails.concat(self.pitch.subscribers.map{ |email| "'#{email}'"}).uniq
+    self.pitch.subscribers.find(:all,:conditions=>"email not in (#{emails.join(',')})").each do |subscriber|
+      Mailer.deliver_story_published_notification(self, subscriber, subscriber.email, subscriber)
+    end
+    #email fact checker if not already covered...
+    if self.pitch && self.pitch.fact_checker_id
+        fact_checker = User.find_by_id(self.pitch.fact_checker_id)
+        if fact_checker && fact_checker.email && emails.include?("'#{fact_checker.email}'")
+            Mailer.deliver_story_published_notification(self,fact_checker.full_name,fact_checker.email)
+        end
+    end
   end
   
   def to_s
