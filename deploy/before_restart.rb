cb4998b9c7417fe4730e0f9329f5394bb2d1d01d
GIT_AUTHOR_NAME <github@sundelof.com>
2010-03-01 13:19:00 -0800

    taking out memcache restart

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,4 +1,4 @@
 run "ln -nfs #{shared_path}/config/settings.yml #{release_path}/config/settings.yml"
 run "ln -nfs #{shared_path}/config/newrelic.yml #{release_path}/config/newrelic.yml"
 run "ln -nfs #{shared_path}/config/s3.yml #{release_path}/config/s3.yml"
-run "sudo /etc/init.d/memcached restart"
+#run "sudo /etc/init.d/memcached restart"
