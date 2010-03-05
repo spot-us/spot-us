e4a5942ac67ed8d38d8eb37e92e02daafb965230
GIT_AUTHOR_NAME <github@sundelof.com>
2010-03-04 18:09:34 -0800

    making sure you can support spotus with credits

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,11 @@ config.action_mailer.raise_delivery_errors = false
 # use_gateway = false
 use_gateway = true
 
+if APP_CONFIG[:action_mailer].is_a?(Hash)
+  config.action_mailer.delivery_method = APP_CONFIG[:action_mailer][:delivery_method]
+  config.action_mailer.smtp_settings   = APP_CONFIG[:action_mailer][:smtp_settings]
+end
+
 ActiveMerchant::Billing::Base.mode = :test
 
 config.to_prepare do
