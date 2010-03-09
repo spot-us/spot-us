89fae7697596136b5dc5510ce0b05b2919e77b0d
erik <github@sundelof.com>
2010-03-09 14:40:30 -0800

    fix

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -83,7 +83,7 @@ class NewsItem < ActiveRecord::Base
     validates_attachment_content_type :featured_image,
       :content_type => ['image/jpeg', 'image/pjpeg', 'image/gif', 'image/png',
                         'image/x-png', 'image/jpg'],
-      :message      => "Oops! Make sure you are uploading an image file.",                  ,
+      :message      => "Oops! Make sure you are uploading an image file.",
       :unless => :featured_image_name
 
     validates_attachment_size :featured_image, :in => 1..5.megabytes
