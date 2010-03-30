043977b0a37420940d3ae84b1c9b2e237723dec4
erik <github@sundelof.com>
2010-03-30 11:02:51 -0700

    changes to the widget

--- public/javascripts/spotus_lite.js
+++ public/javascripts/spotus_lite.js
@@ -1,3 +1,5 @@
 function calcHeight(){ 
-	var the_height=document.getElementById('spotus_lite').contentWindow.document.body.scrollHeight;document.getElementById('spotus_lite').height=the_height;
+	var the_height=document.getElementById('spotus_lite').contentWindow.document.body.scrollHeight;
+	document.getElementById('spotus_lite').height=the_height;
+	document.getElementById('spotus_lite').display='block';
 }
