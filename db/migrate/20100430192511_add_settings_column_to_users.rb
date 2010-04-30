e087465d52599c629237f76ef1a595cdd2e7cc42
erik <github@sundelof.com>
2010-04-30 12:36:34 -0700

    adding settings for facebook and twitter

---
+++ db/migrate/20100430192511_add_settings_column_to_users.rb
@@ -0,0 +1,12 @@
+class AddSettingsColumnToUsers < ActiveRecord::Migration
+  def self.up
+    add_column :users, :notify_facebook_wall, :boolean, :default => false, :after => :notify_comments
+    add_column :users, :notify_twitter, :boolean, :default => false, :after => :notify_facebook_wall
+    User.update_all("notify_facebook_wall = 1, notify_twitter = 1,")
+  end
+
+  def self.down
+    remove_column :users, :notify_facebook_wall
+    remove_column :users, :notify_facebook_twitter
+  end
+end
