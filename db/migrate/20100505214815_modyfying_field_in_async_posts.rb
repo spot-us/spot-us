bcb595a91195b0ffad2cd4fcc5358f8842ed6d43
erik <github@sundelof.com>
2010-05-05 14:50:24 -0700

    fixing bad column name

---
+++ db/migrate/20100505214815_modyfying_field_in_async_posts.rb
@@ -0,0 +1,10 @@
+class ModyfyingFieldInAsyncPosts < ActiveRecord::Migration
+  def self.up
+       rename_column :users, :type, :post_type
+     end
+  end
+
+  def self.down
+    rename_column :users, :post_type, :type
+  end
+end
