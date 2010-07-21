5bc1b4cd4d781c6794415feac33833284048ab51
erik <github@sundelof.com>
2010-07-21 12:18:08 -0700

    making sure the get satisfaction doesnt need to be specified

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,5 +48,5 @@ FACEBOOK_CONSUMER_KEY    	= APP_CONFIG[:facebook][:consumer_key]
 FACEBOOK_CONSUMER_SECRET 	= APP_CONFIG[:facebook][:consumer_secret]
 TWITTER_CONSUMER_KEY    	= APP_CONFIG[:twitter][:consumer_key]
 TWITTER_CONSUMER_SECRET 	= APP_CONFIG[:twitter][:consumer_secret]
-GS_APP						= APP_CONFIG[:get_satisfaction][:app_name]	
+GS_APP						= APP_CONFIG[:get_satisfaction] && APP_CONFIG[:get_satisfaction][:app_name] ? APP_CONFIG[:get_satisfaction][:app_name]: nil
 
