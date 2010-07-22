d33c78a6d08ff34c4a4842f06e4f521be0f29294
erik <github@sundelof.com>
2010-07-22 10:35:01 -0700

    making sure you will see how many credits you have if you have no balance but still have credits

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -95,6 +95,10 @@ module ApplicationHelper
         text << "You have #{number_to_currency(current_user.remaining_credits)} in credits to use toward your donations. "
         text << link_to("Apply Them &raquo;", edit_myspot_donations_amounts_path)
       end
+      if (current_user.total_credits>0 && current_balance == 0)
+        text << "You have #{number_to_currency(current_user.remaining_credits)} in credits to use. "
+        text << link_to("Apply Them &raquo;", edit_myspot_donations_amounts_path)
+      end
       if (current_user.total_credits<=0 && current_balance > 0)
         text << "You need "
         text << link_to(number_to_currency(current_balance), edit_myspot_donations_amounts_path, :id => "current_balance")
