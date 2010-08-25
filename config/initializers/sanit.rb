1abca17a19095dd3074f9bdb9b5f6b2f2c47f991
erik <github@sundelof.com>
2010-08-25 15:22:14 -0700

    adding h2 to allowed tags

--- config/initializers/sanit.rb
+++ config/initializers/sanit.rb
@@ -1,4 +1,4 @@
-ActionView::Base.sanitized_allowed_tags.replace %w(a b em i q blockquote strike strong sub sup tt u ul ol li br p div img object embed param)
+ActionView::Base.sanitized_allowed_tags.replace %w(a b em i q blockquote strike strong sub sup tt u ul ol li br p div img object embed param h2 h3)
 ActionView::Base.sanitized_allowed_attributes.replace  %w(id type width height rel title alt href name value classid codebase allowscriptaccess src allownetworking allowfullscreen data type flashvars bgcolor) 
 
 class String
