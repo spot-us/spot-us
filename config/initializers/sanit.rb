158fd32e41a5231b0fc7b5e48cacbef586511371
erik <github@sundelof.com>
2010-10-27 13:56:25 -0700

    shortening and stripping HTML for the excerpt

--- config/initializers/sanit.rb
+++ config/initializers/sanit.rb
@@ -5,4 +5,13 @@ class String
   def sanitize(options={})
     ActionController::Base.helpers.sanitize(self, options)
   end
+  
+  def strip_html(text)
+    self.gsub(/<\/?[^>]*>/, "")
+  end
+
+  def truncate_words(length = 30, end_string = '&hellip; ')
+    words = self.split()
+    words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
+  end
 end
