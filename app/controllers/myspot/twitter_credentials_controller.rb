306a40b66b77111b4e16f0cf5691db5f1db1bedf
erik <github@sundelof.com>
2011-01-11 10:34:35 -0800

    massive cleanup

--- app/controllers/myspot/twitter_credentials_controller.rb
+++ app/controllers/myspot/twitter_credentials_controller.rb
@@ -39,28 +39,6 @@ class Myspot::TwitterCredentialsController < ApplicationController
     end
   end
   
-  # response_for :create do |format|
-  #   format.html do
-  #     if resource_saved?
-  #       flash[:success] = "You have successfuly linked your twitter credentials to your account!"
-  #       redirect_to myspot_twitter_credentials_path
-  #     else
-  #       render :action => 'new'
-  #     end
-  #   end
-  # end
-  # 
-  # response_for :update do |format|
-  #   format.html do
-  #     if resource_saved?
-  #       flash[:success] = "You have successfully updated your twitter credentials to your account!"
-  #       redirect_to edit_myspot_twitter_credentials_path
-  #     else
-  #       render :action => 'edit'
-  #     end
-  #   end
-  # end
-  
   private
   
   def find_resource
