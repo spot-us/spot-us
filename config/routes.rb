4a8683f758a8d8e89f9700c873ee3152b6bc1cc4
erik <github@sundelof.com>
2011-02-01 21:57:14 -0800

    adding routes for story on pitch page

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :sections
   
   map.resources :pitches, :member => {:begin_story => :get, :feature => :get, :unfeature => :get, :half_fund => :put, :fully_fund => :put, :show_support => :put, :apply_to_contribute => :get, :assign_fact_checker => :put, :blog_posts => :get, :reset_funding => :get} do |pitch|
-    pitch.connect ":tab", :controller => "pitches", :action => "show", :requirements => { :tab => /summary|story|updates|discussion|assignments|donors|details/  }
+    pitch.connect ":tab", :controller => "pitches", :action => "show", :requirements => { :tab => /summary|story|updates|discussion|assignments|donors|details|story/  }
     pitch.resources :posts, :except => [:index], :as => 'updates'
     pitch.resources :comments, :except => [:index, :show]
     pitch.resources :assignments, :except => [:index], :member => {:process_application => :get, :open_assignment => :get, :close_assignment => :get}
