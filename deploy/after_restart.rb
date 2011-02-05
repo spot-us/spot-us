2e99f1b7d692d42a900507276026a4cffea133d8
erik <github@sundelof.com>
2011-02-05 14:07:12 -0800

    reverting again.

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,3 +1,3 @@
-run "cd #{latest_release}; rake ts:stop"
-run "cd #{latest_release}; rake ts:index"
-run "cd #{latest_release}; rake ts:start"
+#run "cd #{latest_release}; rake ts:stop"
+#run "cd #{latest_release}; rake ts:index"
+#run "cd #{latest_release}; rake ts:start"
