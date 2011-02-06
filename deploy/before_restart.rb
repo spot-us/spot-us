74a27165e62266459a41649738503d2e9d96b989
erik <github@sundelof.com>
2011-02-06 11:19:19 -0800

    adding some search engine stuff

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -3,3 +3,5 @@ run "ln -nfs #{shared_path}/config/newrelic.yml #{release_path}/config/newrelic.
 run "ln -nfs #{shared_path}/config/s3.yml #{release_path}/config/s3.yml"
 run "ln -nfs #{shared_path}/config/facebooker.yml #{release_path}/config/facebooker.yml"
 #run "sudo /etc/init.d/memcached restart"
+
+run "cd #{latest_release}; rake ts:stop"
