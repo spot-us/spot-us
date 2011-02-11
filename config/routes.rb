6d9822366ac42f85343c378bb5ed27f4865f8aaf
erik <github@sundelof.com>
2011-02-11 09:48:28 -0800

    fixing the broken story updates

--- config/routes.rb
+++ config/routes.rb
@@ -41,10 +41,10 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :sections
   
   map.resources :pitches, :member => {:begin_story => :get, :feature => :get, :unfeature => :get, :half_fund => :put, :fully_fund => :put, :show_support => :put, :apply_to_contribute => :get, :assign_fact_checker => :put, :blog_posts => :get, :reset_funding => :get} do |pitch|
-    pitch.connect ":tab", :controller => "pitches", :action => "show", :requirements => { :tab => /summary|story|updates|discussion|assignments|donors|details|story/  }
     pitch.resources :posts, :except => [:index], :as => 'updates'
     pitch.resources :comments, :except => [:index, :show]
     pitch.resources :assignments, :except => [:index], :member => {:process_application => :get, :open_assignment => :get, :close_assignment => :get}
+    pitch.connect ":tab", :controller => "pitches", :action => "show", :requirements => { :tab => /summary|story|updates|discussion|assignments|donors|details|story/  }
   end
   map.connect "pitches/:id/blog_posts", :controller => "pitches", :action => "show", :tab=>'posts'
   map.connect "pitches/:id/widget", :controller => "pitches", :action => "widget"
