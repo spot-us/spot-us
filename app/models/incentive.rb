6f3b5cc928972f38bb078563f52245db6d719eb7
erik <github@sundelof.com>
2011-03-18 16:48:36 -0700

    tweaking the incentives

--- app/models/incentive.rb
+++ app/models/incentive.rb
@@ -1,24 +1,17 @@
 class Incentive < ActiveRecord::Base
   
   belongs_to :pitch
-  
-  INCENTIVES = ActiveSupport::OrderedHash.new 
-  INCENTIVES[1] = 10
-  INCENTIVES[2] = 50
-  INCENTIVES[3] = 100
-  INCENTIVES[4] = 150
-  INCENTIVES[5] = 500
-  
-  validates_presence_of :incentive_id, :pitch, :description
+    
+  validates_presence_of :amount, :pitch, :description
   
   def level?
-    "Donated XXX or more".gsub('XXX',"#{INCENTIVES[incentive_id]}".to_currency)
+    "Donate XXX or more".gsub('XXX',"#{amount}".to_currency)
   end
   
   def validate
-    p = Incentive.find(:first, :conditions => ["incentive_id=?", incentive_id])
-    unless p
-      errors.add('', 'You can only have one incentive per donation level')
+    p = Incentive.find(:first, :conditions => ["amount=?", amount])
+    if p
+      errors.add('', 'You can only have one incentive per amount level')
       return false
     end
     return true
--- db/migrate/20110318203647_create_incentives.rb
+++ db/migrate/20110318203647_create_incentives.rb
@@ -1,7 +1,8 @@
 class CreateIncentives < ActiveRecord::Migration
   def self.up
     create_table :incentives do |t|
-      t.integer       :incentive_id, :pitch_id
+      t.integer       :pitch_id
+      t.float         :amount
       t.string        :description
       t.timestamps
     end
