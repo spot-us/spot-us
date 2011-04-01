da466f70080e5b42ace37e1ce070bb65db8ffccd
erik <github@sundelof.com>
2011-03-31 17:43:15 -0700

    adding the right validation call

--- app/models/incentive.rb
+++ app/models/incentive.rb
@@ -3,12 +3,13 @@ class Incentive < ActiveRecord::Base
   belongs_to :pitch
     
   validates_presence_of :amount, :pitch, :description
+  validate_on_create :check_if_amount_is_uniq
   
   def level?
     "Donate XXX or more".gsub('XXX',"#{amount}".to_currency)
   end
   
-  def validate
+  def check_if_amount_is_uniq
     p = nil
     p = Incentive.find(:first, :conditions => ["amount=? and pitch_id=?", amount, pitch.id]) if pitch
     if p
