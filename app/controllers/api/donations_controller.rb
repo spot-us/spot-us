0c1e623ca66f6d132cfd852dcd998411de0fd746
erik <github@sundelof.com>
2011-06-22 17:22:02 -0700

    added a simple donate with card function for the api

--- app/controllers/api/donations_controller.rb
+++ app/controllers/api/donations_controller.rb
@@ -1,2 +1,48 @@
+require 'oauth/controllers/provider_controller'
+
 class Api::DonationsController < ApplicationController
+
+  include OAuth::Controllers::ProviderController
+
+  before_filter :login_or_oauth_required
+
+  def card
+
+    arr = {}
+
+    current_user = current_token.user
+    
+    purchase                 = Purchase.new(params[:purchase])
+    purchase.user            = current_user
+
+    donation_amount = params[:purchase][:donation_amount]
+    spotus_amount = params[:purchase][:donation_amount] * SpotusDonation::SPOTUS_TITHE
+
+    # create the donation and do not run any the limiting to existing donations rules
+    d = Donation.create(:user_id => current_user.id, :pitch_id => params[:pitch_id], :amount => donation_amount, :donation_type => "payment")
+
+    # create the spotus donation
+    spotus_donation = SpotusDonation.create(:user_id => current_user.id, :amount => spotus_amount)
+
+    purchase.donations       = [d]
+    purchase.spotus_donation = spotus_donation
+
+    arr = {}
+
+    begin
+      if purchase.save
+        arr[:success] = "Congratulations! You have donated to the pitch"
+        arr[:id] = d.id
+      else
+        arr[:errors] = purchase.errors.full_messages
+      end
+    rescue ActiveMerchant::ActiveMerchantError, Purchase::GatewayError => e
+      arr[:errors] = [e.message]
+    end
+    
+    render :json => arr
+    return
+
+  end
+
 end
