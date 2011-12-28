8054bf69f2346b8384a7424e4f44d3f2a61cf648
erik <github@sundelof.com>
2011-12-28 12:23:07 -0800

    fixing typo

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -18,7 +18,7 @@ class Api::SearchController < ApplicationController
       i = 1
       until items_found || (starting_length - i + 1)  <= @require_nr_matched_terms
         @items, items_found = get_items(@page, @filter, get_search_term(@terms,i))
-        @matched_terms = search_term(@terms,i) if items_found
+        @matched_terms = search_terms(@terms,i) if items_found
         i += 1
       end
     end
