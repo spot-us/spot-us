3964db74ae1aa7315c5fb9647315838eeaafd2ee
erik <github@sundelof.com>
2011-12-29 14:17:04 -0800

    correcting a bad typo

--- config/initializers/sanit.rb
+++ config/initializers/sanit.rb
@@ -23,11 +23,11 @@ class String
     self.strip_html.truncate_words(length, end_string)
   end
   
-  def shorten_character_limit(length=45, end_string)
-    self.length>length ? self[0..length].gsub(/\w+$/, '')+end_string : self
+  def shorten_character_limit(length, end_string)
+    self.length>length ? self[0..length].gsub(/\w+$/, '') + end_string : self
   end
   
-  def strip_and_shorten_character_limit(length = 30, end_string = '')
+  def strip_and_shorten_character_limit(length = 45, end_string = '')
     self.strip_html.shorten_character_limit(length, end_string)
   end
   
